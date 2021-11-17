7e3c2d4e9d74b4b541e5863bdab66c2f3a6cdeb7
David <contact@jimenez-david.com>
2021-11-17 23:43:25 +0900

    Delete API Services

--- api/controllers/config_controller.go
+++ api/controllers/config_controller.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/numary/ledger/api/models"
+	"github.com/numary/ledger/api/resources"
+	"github.com/spf13/viper"
 )
 
 // ConfigController -
@@ -16,11 +21,19 @@ func NewConfigController() *ConfigController {
 
 // GetInfo -
 func (ctl *ConfigController) GetInfo(c *gin.Context) {
-	// info := ctl.configService.GetConfig()
-	// ctl.responseResource(
-	// 	c,
-	// 	http.StatusOK,
-	// 	info,
-	// 	&resources.Info{},
-	// )
+	ctl.responseResource(
+		c,
+		http.StatusOK,
+		&models.Infos{
+			Server:  "numary-ledger",
+			Version: viper.Get("version"),
+			Config: &models.Config{
+				LedgerStorage: &models.LedgerStorage{
+					Driver:  viper.Get("storage.driver"),
+					Ledgers: viper.Get("ledgers"),
+				},
+			},
+		},
+		&resources.Info{},
+	)
 }
