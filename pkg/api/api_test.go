6a914122826985f0cdb41dd8228b26eb39819d3c
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-01-21 17:09:17 +0100

    Add a bunch of test on transactions commit.

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/numary/ledger/pkg/api/controllers"
 	"github.com/numary/ledger/pkg/api/routes"
+	"github.com/numary/ledger/pkg/core"
 	"github.com/numary/ledger/pkg/ledger"
 	"github.com/numary/ledger/pkg/storage"
 	"github.com/numary/ledger/pkg/storage/sqlstorage"
@@ -75,4 +78,157 @@ func TestAdditionalPerLedgerMiddleware(t *testing.T) {
 			assert.Equal(t, 418, rec.Code)
 		}),
 	)
+}
+
+func TestCommitTransaction(t *testing.T) {
+
+	type testCase struct {
+		name               string
+		transactions       []core.Transaction
+		expectedStatusCode int
+	}
+	testCases := []testCase{
+		{
+			name:               "nominal",
+			expectedStatusCode: http.StatusOK,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "central_bank",
+							Amount:      1000,
+							Asset:       "USB",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:               "no-postings",
+			expectedStatusCode: http.StatusBadRequest,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{},
+				},
+			},
+		},
+		{
+			name:               "negative-amount",
+			expectedStatusCode: http.StatusBadRequest,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "central_bank",
+							Amount:      -1000,
+							Asset:       "USB",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:               "wrong-asset",
+			expectedStatusCode: http.StatusBadRequest,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "central_bank",
+							Amount:      1000,
+							Asset:       "@TOK",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:               "bad-address",
+			expectedStatusCode: http.StatusBadRequest,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "#fake",
+							Amount:      1000,
+							Asset:       "TOK",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:               "missing-funds",
+			expectedStatusCode: http.StatusBadRequest,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "foo",
+							Destination: "bar",
+							Amount:      1000,
+							Asset:       "TOK",
+						},
+					},
+				},
+			},
+		},
+		{
+			name:               "reference-conflict",
+			expectedStatusCode: http.StatusConflict,
+			transactions: []core.Transaction{
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "bar",
+							Amount:      1000,
+							Asset:       "TOK",
+						},
+					},
+					Reference: "ref",
+				},
+				{
+					Postings: core.Postings{
+						{
+							Source:      "world",
+							Destination: "bar",
+							Amount:      1000,
+							Asset:       "TOK",
+						},
+					},
+					Reference: "ref",
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			withNewModule(t, fx.Invoke(func(api *API) {
+				doRequest := func(tx core.Transaction) *httptest.ResponseRecorder {
+					data, err := json.Marshal(tx)
+					assert.NoError(t, err)
+
+					rec := httptest.NewRecorder()
+					req := httptest.NewRequest(http.MethodPost, "/quickstart/transactions", bytes.NewBuffer(data))
+					req.Header.Set("Content-Type", "application/json")
+
+					api.ServeHTTP(rec, req)
+					return rec
+				}
+				for i := 0; i < len(tc.transactions)-1; i++ {
+					rsp := doRequest(tc.transactions[i])
+					assert.Equal(t, http.StatusOK, rsp.Result().StatusCode)
+				}
+				rsp := doRequest(tc.transactions[len(tc.transactions)-1])
+				assert.Equal(t, tc.expectedStatusCode, rsp.Result().StatusCode)
+			}))
+
+		})
+	}
 }
