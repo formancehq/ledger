c22c43b1c189afec650702aa2e6042fd89cb0b67
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-01-22 11:45:30 +0100

    Dispatch api test file.

--- pkg/api/controllers/transaction_controller_test.go
+++ pkg/api/controllers/transaction_controller_test.go
@@ -1,84 +1,18 @@
-package api
+package controllers_test
 
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
-	"github.com/gin-gonic/gin"
+	"github.com/numary/ledger/pkg/api"
 	"github.com/numary/ledger/pkg/api/controllers"
-	"github.com/numary/ledger/pkg/api/routes"
+	"github.com/numary/ledger/pkg/api/internal"
 	"github.com/numary/ledger/pkg/core"
-	"github.com/numary/ledger/pkg/ledger"
-	"github.com/numary/ledger/pkg/storage"
-	"github.com/numary/ledger/pkg/storage/sqlstorage"
-	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/fx"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
-
-func withNewModule(t *testing.T, options ...fx.Option) {
-	module := Module(Config{
-		StorageDriver: viper.GetString("sqlite"),
-		LedgerLister: controllers.LedgerListerFn(func(r *http.Request) []string {
-			return []string{}
-		}),
-		Version: "latest",
-	})
-	ch := make(chan struct{})
-	options = append([]fx.Option{
-		module,
-		ledger.ResolveModule(),
-		storage.DefaultModule(),
-		sqlstorage.TestingModule(),
-	}, options...)
-	options = append(options, fx.Invoke(func() {
-		close(ch)
-	}))
-
-	fx.New(options...)
-	select {
-	case <-ch:
-	default:
-		assert.Fail(t, "something went wrong")
-	}
-}
-
-func TestAdditionalGlobalMiddleware(t *testing.T) {
-	withNewModule(t,
-		routes.ProvideGlobalMiddleware(func() gin.HandlerFunc {
-			return func(context *gin.Context) {
-				context.AbortWithError(418, errors.New(""))
-			}
-		}),
-		fx.Invoke(func(api *API) {
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, "/_info", nil)
-
-			api.ServeHTTP(rec, req)
-			assert.Equal(t, 418, rec.Code)
-		}),
-	)
-}
-
-func TestAdditionalPerLedgerMiddleware(t *testing.T) {
-	withNewModule(t,
-		routes.ProvidePerLedgerMiddleware(func() gin.HandlerFunc {
-			return func(context *gin.Context) {
-				context.AbortWithError(418, errors.New(""))
-			}
-		}),
-		fx.Invoke(func(api *API) {
-			rec := httptest.NewRecorder()
-			req := httptest.NewRequest(http.MethodGet, "/XXX/transactions", nil)
-
-			api.ServeHTTP(rec, req)
-			assert.Equal(t, 418, rec.Code)
-		}),
-	)
-}
 
 func TestCommitTransaction(t *testing.T) {
 
@@ -216,7 +150,7 @@ func TestCommitTransaction(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			withNewModule(t, fx.Invoke(func(api *API) {
+			opentelemetry.WithNewModule(t, fx.Invoke(func(api *api.API) {
 				doRequest := func(tx core.Transaction) *httptest.ResponseRecorder {
 					data, err := json.Marshal(tx)
 					assert.NoError(t, err)
