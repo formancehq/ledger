3e195d646b43095b9113012a155a483b149a4dde
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-01-24 11:15:31 +0100

    Add some test on script controller.

--- pkg/api/controllers/script_controller.go
+++ pkg/api/controllers/script_controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"github.com/davecgh/go-spew/spew"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -55,6 +56,8 @@ func (ctl *ScriptController) PostScript(c *gin.Context) {
 
 	var script core.Script
 	c.ShouldBind(&script)
+
+	spew.Dump(script)
 
 	err := l.(*ledger.Ledger).Execute(c.Request.Context(), script)
 
