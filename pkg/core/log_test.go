c169c2ec118fb0be0ace3e11d1e283226d3665fa
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-03-30 11:00:26 +0200

    Use UTC date (pipeline failed)

--- pkg/core/log_test.go
+++ pkg/core/log_test.go
@@ -8,11 +8,11 @@ import (
 
 func TestLog(t *testing.T) {
 
-	d := time.Unix(1648542028, 0)
+	d := time.Unix(1648542028, 0).UTC()
 
 	log1 := NewTransactionLogWithDate(nil, Transaction{}, d)
 	log2 := NewTransactionLogWithDate(&log1, Transaction{}, d)
-	if !assert.Equal(t, "c92c4ef0d943f91d326c9241bd4de5766b778671969c861dda798a79e5616f44", log2.Hash) {
+	if !assert.Equal(t, "69595c585c2068dea91e0a918f49e3e00a2e22da54af4b6a506ff57a4f600485", log2.Hash) {
 		return
 	}
 }
