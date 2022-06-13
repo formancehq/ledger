6a478269c1cb0001f6288950a33b90c6e3544277
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2022-06-13 19:07:47 +0200

    Update pkg/storage/sqlstorage/aggregations.go
    Co-authored-by: Antoine Gelloz <antoine.gelloz@me.com>

--- pkg/storage/sqlstorage/aggregations.go
+++ pkg/storage/sqlstorage/aggregations.go
@@ -100,8 +100,7 @@ func (s *Store) getAccountVolume(ctx context.Context, exec executor, address, as
 		input  int64
 		output int64
 	)
-	err := row.Scan(&input, &output)
-	if err != nil {
+	if err := row.Scan(&input, &output);  err != nil {
 		if err == sql.ErrNoRows {
 			return core.Volume{}, nil
 		}
