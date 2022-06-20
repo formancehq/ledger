91d9d6f258515134cdbd173c07eb326189feca3f
antoinegelloz <antoine.gelloz@me.com>
2022-06-20 10:37:15 +0200

    lint

--- pkg/core/account.go
+++ pkg/core/account.go
@@ -5,9 +5,9 @@ const (
 )
 
 type Account struct {
-	Address  string        `json:"address" example:"users:001"`
+	Address  string         `json:"address" example:"users:001"`
 	Type     string         `json:"type,omitempty" example:"virtual"`
 	Balances AssetsBalances `json:"balances,omitempty" example:"COIN:100"`
 	Volumes  AssetsVolumes  `json:"volumes,omitempty"`
-	Metadata Metadata      `json:"metadata" swaggertype:"object"`
+	Metadata Metadata       `json:"metadata" swaggertype:"object"`
 }
