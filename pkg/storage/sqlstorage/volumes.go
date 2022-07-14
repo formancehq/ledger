485ac1441e97bbd9ddde4a35602a8090b5c7ba8a
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-07-14 20:40:45 +0200

    refactor: clean commented code

--- pkg/storage/sqlstorage/volumes.go
+++ pkg/storage/sqlstorage/volumes.go
@@ -7,29 +7,10 @@ import (
 	"github.com/numary/ledger/pkg/core"
 )
 
-/**
-
-INSERT INTO "VAR_LEDGER_NAME".volumes (account, asset, input, output)
-VALUES (p.source, p.asset, 0, p.amount::bigint)
-ON CONFLICT (account, asset) DO UPDATE SET output = p.amount::bigint + (
-	SELECT output
-	FROM "VAR_LEDGER_NAME".volumes
-	WHERE account = p.source
-	  AND asset = p.asset
-);
-*/
-
 func (s *Store) updateVolumes(ctx context.Context, exec executor, volumes core.AccountsAssetsVolumes) error {
 
 	for account, accountVolumes := range volumes {
 		for asset, volumes := range accountVolumes {
-			//ub := sqlbuilder.NewUpdateBuilder()
-			//sqlq, args := ub.
-			//	Update(s.schema.Table("volumes")).
-			//	Set(ub.Assign("input", volumes.Input), ub.Assign("output", volumes.Output)).
-			//	Where(ub.And(ub.E("account", account), ub.E("asset", asset))).
-			//	BuildWithFlavor(s.schema.Flavor())
-
 			ib := sqlbuilder.NewInsertBuilder()
 			inputArg := ib.Var(volumes.Input)
 			outputArg := ib.Var(volumes.Output)
