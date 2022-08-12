a9cd8d88485d37e673f85928e6549879e4eb3a47
Clément Salaün <salaun.clement@gmail.com>
2022-08-12 11:51:59 +0200

    feat: add past timestamps flag (#314)

--- pkg/ledger/process_test.go
+++ pkg/ledger/process_test.go
@@ -305,8 +305,37 @@ func TestLedger_processTx(t *testing.T) {
 					Timestamp: now.Add(-time.Second),
 				},
 			})
+
 			assert.Error(t, err)
 			assert.True(t, IsValidationError(err))
 		})
 	})
+	runOnLedger(func(l *Ledger) {
+		t.Run("date in the past (allowed by policy)", func(t *testing.T) {
+			now := time.Now()
+			require.NoError(t, l.store.Commit(context.Background(), core.ExpandedTransaction{
+				Transaction: core.Transaction{
+					TransactionData: core.TransactionData{
+						Timestamp: now,
+					},
+					ID: 0,
+				},
+			}))
+
+			_, err := l.processTx(context.Background(), []core.TransactionData{
+				{
+					Postings: []core.Posting{{
+						Source:      "world",
+						Destination: "bank",
+						Amount:      100,
+						Asset:       "USD",
+					}},
+					Timestamp: now.Add(-time.Second),
+				},
+			})
+
+			assert.NoError(t, err)
+
+		})
+	}, WithPastTimestamps)
 }
