62cbea47dde03464a70e25d9d2e31074ebc77e0e
Antoine Gelloz <antoine.gelloz@me.com>
2022-09-05 17:54:31 +0200

    fix: lint

--- cmd/internal/utils.go
+++ cmd/internal/utils.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 )
@@ -18,10 +17,4 @@ func setEnvVar(key, value string) func() {
 	return func() {
 		os.Setenv(prefixedFlag, oldEnv)
 	}
-}
-
-type roundTripperFn func(req *http.Request) (*http.Response, error)
-
-func (fn roundTripperFn) RoundTrip(req *http.Request) (*http.Response, error) {
-	return fn(req)
 }
