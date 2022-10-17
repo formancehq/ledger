e5a075e116f40cb6240bb33642fac5f8b373c4b6
Antoine Gelloz <antoine.gelloz@me.com>
2022-10-17 18:41:14 +0200

    feat: improve bench

--- pkg/storage/sqlstorage/transactions_test.go
+++ pkg/storage/sqlstorage/transactions_test.go
@@ -2,10 +2,14 @@ package sqlstorage_test
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/numary/go-libs/sharedapi"
 	"github.com/numary/go-libs/sharedlogging"
 	"github.com/numary/go-libs/sharedlogging/sharedlogginglogrus"
 	"github.com/numary/ledger/pkg/core"
@@ -45,7 +49,7 @@ func BenchmarkStore_GetTransactions(b *testing.B) {
 						return err
 					}
 
-					getTransactions(b, store, 12, 500)
+					benchGetTransactions(b, store)
 					return nil
 				},
 			})
@@ -57,11 +61,13 @@ func BenchmarkStore_GetTransactions(b *testing.B) {
 	}(app, context.Background())
 }
 
-func getTransactions(b *testing.B, store *sqlstorage.Store, pages, pageSize int) {
+func benchGetTransactions(b *testing.B, store *sqlstorage.Store) {
+	maxPages := 120
+	maxPageSize := 500
 	id := uint64(0)
 	var txs []core.ExpandedTransaction
-	for i := 0; i < pages; i++ {
-		for j := 0; j < pageSize; j++ {
+	for i := 0; i < maxPages; i++ {
+		for j := 0; j < maxPageSize; j++ {
 			acc := uuid.NewString() + ":key1:" + uuid.NewString() + ":key2:" + uuid.NewString()
 			txs = append(txs, core.ExpandedTransaction{
 				Transaction: core.Transaction{
@@ -119,12 +125,34 @@ func getTransactions(b *testing.B, store *sqlstorage.Store, pages, pageSize int)
 		require.NoError(b, store.Commit(context.Background(), txs...))
 	}
 
+	numTxs := maxPages * maxPageSize
 	nb, err := store.CountTransactions(context.Background(), ledger.TransactionsQuery{})
 	require.NoError(b, err)
-	require.Equal(b, uint64(pages*pageSize), nb)
+	require.Equal(b, uint64(numTxs), nb)
 
 	b.ResetTimer()
 	b.StartTimer()
+
+	b.Run(fmt.Sprintf("only first page of size %d out of %d txs", 1, numTxs),
+		func(b *testing.B) { getFirstPage(b, store, 1) })
+	b.Run(fmt.Sprintf("only first page of size %d out of %d txs", 10, numTxs),
+		func(b *testing.B) { getFirstPage(b, store, 10) })
+	b.Run(fmt.Sprintf("only first page of size %d out of %d txs", 50, numTxs),
+		func(b *testing.B) { getFirstPage(b, store, 50) })
+	b.Run(fmt.Sprintf("only first page of size %d out of %d txs", 500, numTxs),
+		func(b *testing.B) { getFirstPage(b, store, 500) })
+
+	b.Run(fmt.Sprintf("all pages of size %d out of %d txs", 1, numTxs),
+		func(b *testing.B) { getAllPages(b, store, 1, maxPages*maxPageSize) })
+	b.Run(fmt.Sprintf("all pages of size %d out of %d txs", 10, numTxs),
+		func(b *testing.B) { getAllPages(b, store, 10, maxPages*maxPageSize) })
+	b.Run(fmt.Sprintf("all pages of size %d out of %d txs", 50, numTxs),
+		func(b *testing.B) { getAllPages(b, store, 50, maxPages*maxPageSize) })
+	b.Run(fmt.Sprintf("all pages of size %d out of %d txs", 500, numTxs),
+		func(b *testing.B) { getAllPages(b, store, 500, maxPages*maxPageSize) })
+}
+
+func getFirstPage(b *testing.B, store *sqlstorage.Store, pageSize int) {
 	for n := 0; n < b.N; n++ {
 		cursor, err := store.GetTransactions(context.Background(), ledger.TransactionsQuery{
 			Filters: ledger.TransactionsQueryFilters{
@@ -135,5 +163,49 @@ func getTransactions(b *testing.B, store *sqlstorage.Store, pages, pageSize int)
 		require.NoError(b, err)
 		require.Equal(b, pageSize, cursor.PageSize)
 		require.Len(b, cursor.Data, pageSize)
+	}
+}
+
+func getAllPages(b *testing.B, store *sqlstorage.Store, pageSize, maxNumTxs int) {
+	for n := 0; n < b.N; n++ {
+		numTxs := 0
+		var txQuery *ledger.TransactionsQuery
+		cursor := sharedapi.Cursor[core.ExpandedTransaction]{
+			HasMore: true,
+		}
+		var err error
+		for cursor.HasMore {
+			if cursor.Next != "" {
+				res, decErr := base64.RawURLEncoding.DecodeString(cursor.Next)
+				if decErr != nil {
+					return
+				}
+
+				token := sqlstorage.TxsPaginationToken{}
+				if err = json.Unmarshal(res, &token); err != nil {
+					return
+				}
+
+				txQuery = ledger.NewTransactionsQuery().
+					WithAfterTxID(token.AfterTxID).
+					WithSourceFilter(token.SourceFilter).
+					WithPageSize(token.PageSize)
+			} else {
+				txQuery = &ledger.TransactionsQuery{
+					Filters: ledger.TransactionsQueryFilters{
+						Source: ".*:key1:.*:key2:.*",
+					},
+					PageSize: uint(pageSize),
+				}
+			}
+
+			cursor, err = store.GetTransactions(context.Background(), *txQuery)
+			require.NoError(b, err)
+			require.Equal(b, pageSize, cursor.PageSize)
+			require.Len(b, cursor.Data, pageSize)
+			numTxs += len(cursor.Data)
+		}
+
+		require.Equal(b, maxNumTxs, numTxs)
 	}
 }
