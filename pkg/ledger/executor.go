c811eb08179ce2935f7e10a0db9d3559794df296
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-01-27 16:46:33 +0100

    feat: add traces

--- pkg/ledger/executor.go
+++ pkg/ledger/executor.go
@@ -17,7 +17,6 @@ import (
 	"github.com/numary/ledger/pkg/opentelemetry"
 	"github.com/numary/ledger/pkg/storage"
 	"github.com/pkg/errors"
-	"go.opentelemetry.io/otel/attribute"
 )
 
 func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, scripts ...core.ScriptData) ([]core.ExpandedTransaction, error) {
@@ -32,11 +31,14 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 
 	addOps := new(core.AdditionalOperations)
 
-	lastTx, err := l.store.GetLastTransaction(ctx)
+	subContext, span := opentelemetry.Start(ctx, "Get last transaction")
+	lastTx, err := l.store.GetLastTransaction(subContext)
 	if err != nil {
+		span.End()
 		return []core.ExpandedTransaction{}, errors.Wrap(err,
 			"could not get last transaction")
 	}
+	span.End()
 
 	vAggr := NewVolumeAggregator(l)
 	txs := make([]core.ExpandedTransaction, 0)
@@ -48,8 +50,10 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 	}
 	contracts := make([]core.Contract, 0)
 	if checkMapping {
-		mapping, err := l.store.LoadMapping(ctx)
+		subContext, span := opentelemetry.Start(ctx, "Load mapping")
+		mapping, err := l.store.LoadMapping(subContext)
 		if err != nil {
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err,
 				"loading mapping")
 		}
@@ -57,6 +61,7 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 			contracts = append(contracts, mapping.Contracts...)
 		}
 		contracts = append(contracts, DefaultContracts...)
+		span.End()
 	}
 
 	usedReferences := make(map[string]struct{})
@@ -83,42 +88,49 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 		}
 		lastTxTimestamp = script.Timestamp
 
+		subContext, span := opentelemetry.Start(ctx, "Check reference")
 		if script.Reference != "" {
 			if _, ok := usedReferences[script.Reference]; ok {
+				span.End()
 				return []core.ExpandedTransaction{}, NewConflictError()
 			}
 			usedReferences[script.Reference] = struct{}{}
 
-			txs, err := l.GetTransactions(ctx, *NewTransactionsQuery().
+			txs, err := l.GetTransactions(subContext, *NewTransactionsQuery().
 				WithReferenceFilter(script.Reference))
 			if err != nil {
+				span.End()
 				return []core.ExpandedTransaction{}, errors.Wrap(err, "GetTransactions")
 			}
 			if len(txs.Data) > 0 {
+				span.End()
 				return []core.ExpandedTransaction{}, NewConflictError()
 			}
 		}
+		span.End()
 
 		if script.Plain == "" {
 			return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorNoScript,
 				"no script to execute")
 		}
 
+		_, span = opentelemetry.Start(ctx, "Compute hash")
 		h := sha256.New()
 		if _, err := h.Write([]byte(script.Plain)); err != nil {
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err, "hashing script")
 		}
 		curr := h.Sum(nil)
+		span.End()
 
 		var m *vm.Machine
 		if cachedP, found := l.cache.Get(curr); found {
 			logging.Debugf("Ledger.Execute: Numscript found in cache: %x", curr)
 			m = vm.NewMachine(cachedP.(program.Program))
 		} else {
-			compileStartAt := time.Now()
+			_, span = opentelemetry.Start(ctx, "Compile numscript")
 			newP, err := compiler.Compile(script.Plain)
-			compileTerminatedAt := time.Now()
-			span.SetAttributes(attribute.Int("compilation-duration", int(compileTerminatedAt.Sub(compileStartAt).Seconds())))
+			span.End()
 			if err != nil {
 				return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 					err.Error())
@@ -129,7 +141,9 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 					fmt.Errorf("error while calculating the size in bytes of script: %s",
 						script.Plain).Error())
 			}
+			_, span = opentelemetry.Start(ctx, "Store cache value")
 			ok := l.cache.Set(curr, *newP, int64(s))
+			span.End()
 			logging.Debugf("Ledger.Execute: Numscript NOT found in cache (size %d, set attempt returned %v): %x", s, ok, curr)
 			m = vm.NewMachine(*newP)
 		}
@@ -139,19 +153,23 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				errors.Wrap(err, "could not set variables").Error())
 		}
 
+		subContext, span = opentelemetry.Start(ctx, "Resolve resources")
 		resourcesChan, err := m.ResolveResources()
 		if err != nil {
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err,
 				"could not resolve program resources")
 		}
 		for req := range resourcesChan {
 			if req.Error != nil {
+				span.End()
 				return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 					errors.Wrap(req.Error, "could not resolve program resources").Error())
 			}
 			if _, ok := accs[req.Account]; !ok {
-				accs[req.Account], err = l.GetAccount(ctx, req.Account)
+				accs[req.Account], err = l.GetAccount(subContext, req.Account)
 				if err != nil {
+					span.End()
 					return []core.ExpandedTransaction{}, errors.Wrap(err,
 						fmt.Sprintf("could not get account %q", req.Account))
 				}
@@ -159,15 +177,18 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 			if req.Key != "" {
 				entry, ok := accs[req.Account].Metadata[req.Key]
 				if !ok {
+					span.End()
 					return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 						fmt.Sprintf("missing key %v in metadata for account %v", req.Key, req.Account))
 				}
 				data, err := json.Marshal(entry)
 				if err != nil {
+					span.End()
 					return []core.ExpandedTransaction{}, errors.Wrap(err, "json.Marshal")
 				}
 				value, err := machine.NewValueFromTypedJSON(data)
 				if err != nil {
+					span.End()
 					return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 						errors.Wrap(err, fmt.Sprintf(
 							"invalid format for metadata at key %v for account %v",
@@ -179,6 +200,7 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				resp := machine.MonetaryInt(*amt)
 				req.Response <- &resp
 			} else {
+				span.End()
 				return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 					errors.Wrap(err, fmt.Sprintf("invalid ResourceRequest: %+v", req)).Error())
 			}
@@ -186,18 +208,21 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 
 		balanceCh, err := m.ResolveBalances()
 		if err != nil {
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err,
 				"could not resolve balances")
 		}
 		for req := range balanceCh {
 			if req.Error != nil {
+				span.End()
 				return []core.ExpandedTransaction{}, NewScriptError(ScriptErrorCompilationFailed,
 					errors.Wrap(req.Error, "could not resolve program balances").Error())
 			}
 			var amt *core.MonetaryInt
 			if _, ok := accs[req.Account]; !ok {
-				accs[req.Account], err = l.GetAccount(ctx, req.Account)
+				accs[req.Account], err = l.GetAccount(subContext, req.Account)
 				if err != nil {
+					span.End()
 					return []core.ExpandedTransaction{}, errors.Wrap(err,
 						fmt.Sprintf("could not get account %q", req.Account))
 				}
@@ -206,12 +231,16 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 			resp := machine.MonetaryInt(*amt)
 			req.Response <- &resp
 		}
+		span.End()
 
+		_, span = opentelemetry.Start(ctx, "Run machine")
 		exitCode, err := m.Execute()
 		if err != nil {
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err,
 				"script execution failed")
 		}
+		span.End()
 
 		if exitCode != vm.EXIT_OK {
 			switch exitCode {
@@ -237,12 +266,14 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				NewValidationError("transaction has no postings")
 		}
 
+		subContext, span = opentelemetry.Start(ctx, "Aggregate post/pre commit volumes")
 		txVolumeAggr := vAggr.NextTx()
 		postings := make([]core.Posting, len(m.Postings))
 		for j, posting := range m.Postings {
 			amt := core.MonetaryInt(*posting.Amount)
-			if err := txVolumeAggr.Transfer(ctx,
+			if err := txVolumeAggr.Transfer(subContext,
 				posting.Source, posting.Destination, posting.Asset, &amt, accs); err != nil {
+				span.End()
 				return []core.ExpandedTransaction{}, NewTransactionCommitError(i, err)
 			}
 			postings[j] = core.Posting{
@@ -252,11 +283,14 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				Asset:       posting.Asset,
 			}
 		}
+		span.End()
 
+		subContext, span = opentelemetry.Start(ctx, "Check business rules (mapping)")
 		for account, volumes := range txVolumeAggr.PostCommitVolumes {
 			if _, ok := accs[account]; !ok {
-				accs[account], err = l.GetAccount(ctx, account)
+				accs[account], err = l.GetAccount(subContext, account)
 				if err != nil {
+					span.End()
 					return []core.ExpandedTransaction{}, NewTransactionCommitError(i,
 						errors.Wrap(err, fmt.Sprintf("GetAccount '%s'", account)))
 				}
@@ -279,6 +313,7 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 							Metadata: accs[account].Metadata,
 							Asset:    asset,
 						}); !ok {
+							span.End()
 							return []core.ExpandedTransaction{}, NewInsufficientFundError(asset)
 						}
 						break
@@ -286,6 +321,7 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				}
 			}
 		}
+		span.End()
 
 		metadata := m.GetTxMetaJSON()
 		for k, v := range metadata {
@@ -346,11 +382,14 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 		return txs, nil
 	}
 
-	if err := l.store.Commit(ctx, txs...); err != nil {
+	newContext, span := opentelemetry.Start(ctx, "Persist data")
+	if err := l.store.Commit(newContext, txs...); err != nil {
 		switch {
 		case storage.IsErrorCode(err, storage.ConstraintFailed):
+			span.End()
 			return []core.ExpandedTransaction{}, NewConflictError()
 		default:
+			span.End()
 			return []core.ExpandedTransaction{}, errors.Wrap(err,
 				"committing transactions")
 		}
@@ -360,12 +399,15 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 		for addr, m := range addOps.SetAccountMeta {
 			if err := l.store.UpdateAccountMetadata(ctx,
 				addr, m, time.Now().Round(time.Second).UTC()); err != nil {
+				span.End()
 				return []core.ExpandedTransaction{}, errors.Wrap(err,
 					"updating account metadata")
 			}
 		}
 	}
+	span.End()
 
+	ctx, span = opentelemetry.Start(ctx, "Fire events")
 	l.monitor.CommittedTransactions(ctx, l.store.Name(), txs...)
 	if addOps != nil && addOps.SetAccountMeta != nil {
 		for addr, m := range addOps.SetAccountMeta {
@@ -373,6 +415,7 @@ func (l *Ledger) Execute(ctx context.Context, checkMapping, preview bool, script
 				l.store.Name(), core.MetaTargetTypeAccount, addr, m)
 		}
 	}
+	span.End()
 
 	return txs, nil
 }
