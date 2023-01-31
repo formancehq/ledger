aefded5d7d47416c6903aa1e4d5bba81cd195765
Maxence Maireaux <maxence@maireaux.fr>
2023-01-31 10:22:19 +0100

    feat: Remove search

--- components/search/main.go
+++
@@ -1,8 +0,0 @@
-//go:generate docker run --rm -w /local -v ${PWD}:/local openapitools/openapi-generator-cli:latest validate -i ./swagger.yaml
-package main
-
-import "github.com/formancehq/search/cmd"
-
-func main() {
-	cmd.Execute()
-}
