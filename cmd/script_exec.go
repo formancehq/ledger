6d1f4bec7112039409e77ab6f71a526e68c30fc1
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-02-22 17:43:57 +0100

    fix: tests with postgres server (#86)

--- cmd/script_exec.go
+++ cmd/script_exec.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/numary/ledger/pkg/api/controllers"
+	"github.com/pkg/errors"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -24,10 +25,10 @@ func NewScriptExec() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:  "exec [ledger] [script]",
 		Args: cobra.ExactArgs(2),
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
 			b, err := os.ReadFile(args[1])
 			if err != nil {
-				logrus.Fatal(err)
+				return err
 			}
 
 			r := regexp.MustCompile(`^\n`)
@@ -38,7 +39,7 @@ func NewScriptExec() *cobra.Command {
 				"plain": s,
 			})
 			if err != nil {
-				logrus.Fatal(err)
+				return err
 			}
 
 			logrus.Debugln(string(b))
@@ -47,7 +48,7 @@ func NewScriptExec() *cobra.Command {
 				viper.Get(serverHttpBindAddressFlag),
 				args[0]), bytes.NewReader(b))
 			if err != nil {
-				logrus.Fatal(err)
+				return err
 			}
 
 			values := url.Values{}
@@ -59,21 +60,21 @@ func NewScriptExec() *cobra.Command {
 
 			res, err := http.DefaultClient.Do(req)
 			if err != nil {
-				logrus.Fatal(err)
+				return err
 			}
 
 			result := controllers.ScriptResponse{}
 			err = json.NewDecoder(res.Body).Decode(&result)
 			if err != nil {
-				logrus.Fatal(err)
+				return err
 			}
 
 			if result.ErrorCode != "" {
 				switch result.ErrorCode {
 				case "INTERNAL":
-					logrus.Fatal("unexpected error occured")
+					return errors.New("unexpected error occurred")
 				default:
-					logrus.Fatal(result.ErrorCode, result.ErrorMessage)
+					return fmt.Errorf("unexpected error: %s", result.ErrorMessage)
 				}
 			}
 
@@ -96,6 +97,7 @@ func NewScriptExec() *cobra.Command {
 					args[0],
 					result.Transaction.ID)
 			}
+			return nil
 		},
 	}
 	cmd.Flags().Bool(previewFlag, false, "Preview mode (does not save transactions)")
