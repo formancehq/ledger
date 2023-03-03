76715199e4f62142a5037e60958e102e281f02f5
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-03-03 12:48:17 +0100

    chore: remove http basic authentication and deprecated telemetry flags (#106)

--- pkg/api/routes/routes.go
+++ pkg/api/routes/routes.go
@@ -15,23 +15,15 @@ import (
 )
 
 const GlobalMiddlewaresKey = `name:"_routesGlobalMiddlewares" optional:"true"`
-const PerLedgerMiddlewaresKey = `name:"_perLedgerMiddlewares" optional:"true"`
 
 var Module = fx.Options(
 	fx.Provide(
-		fx.Annotate(NewRoutes, fx.ParamTags(GlobalMiddlewaresKey, PerLedgerMiddlewaresKey)),
+		fx.Annotate(NewRoutes, fx.ParamTags(GlobalMiddlewaresKey)),
 	),
 )
 
 func ProvideMiddlewares(provider interface{}, additionalAnnotations ...fx.Annotation) fx.Option {
 	opts := []fx.Annotation{fx.ResultTags(GlobalMiddlewaresKey)}
-	return fx.Provide(
-		fx.Annotate(provider, append(opts, additionalAnnotations...)...),
-	)
-}
-
-func ProvidePerLedgerMiddleware(provider interface{}, additionalAnnotations ...fx.Annotation) fx.Option {
-	opts := []fx.Annotation{fx.ResultTags(PerLedgerMiddlewaresKey)}
 	return fx.Provide(
 		fx.Annotate(provider, append(opts, additionalAnnotations...)...),
 	)
@@ -47,14 +39,12 @@ type Routes struct {
 	balanceController     controllers.BalanceController
 	transactionController controllers.TransactionController
 	globalMiddlewares     []func(handler http.Handler) http.Handler
-	perLedgerMiddlewares  []func(handler http.Handler) http.Handler
 	idempotencyStore      storage.Driver[idempotency.Store]
 	locker                middlewares.Locker
 }
 
 func NewRoutes(
 	globalMiddlewares []func(handler http.Handler) http.Handler,
-	perLedgerMiddlewares []func(handler http.Handler) http.Handler,
 	resolver *ledger.Resolver,
 	ledgerMiddleware middlewares.LedgerMiddleware,
 	configController controllers.ConfigController,
@@ -68,7 +58,6 @@ func NewRoutes(
 ) *Routes {
 	return &Routes{
 		globalMiddlewares:     globalMiddlewares,
-		perLedgerMiddlewares:  perLedgerMiddlewares,
 		resolver:              resolver,
 		ledgerMiddleware:      ledgerMiddleware,
 		configController:      configController,
@@ -101,7 +90,6 @@ func (r *Routes) Engine() *chi.Mux {
 					handler.ServeHTTP(w, r)
 				})
 			})
-			router.Use(r.perLedgerMiddlewares...)
 			router.Use(r.ledgerMiddleware.LedgerMiddleware())
 
 			// LedgerController
