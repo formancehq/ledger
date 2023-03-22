646095b8cf4ac070b7b8d1dd5027088c9109d351
Antoine Gelloz <antoine.gelloz@me.com>
2023-03-22 10:08:41 +0100

    chore: remove deprecated ledger openapi (#152)
    Co-authored-by: Ragot Geoffrey <geoffrey.ragot@gmail.com>

--- pkg/ledger/runner/error.go
+++ pkg/ledger/runner/error.go
@@ -6,58 +6,6 @@ import (
 	"github.com/pkg/errors"
 )
 
-type TransactionCommitError struct {
-	TXIndex int   `json:"index"`
-	Err     error `json:"error"`
-}
-
-func (e TransactionCommitError) Unwrap() error {
-	return e.Err
-}
-
-func (e TransactionCommitError) Error() string {
-	return errors.Wrapf(e.Err, "processing tx %d", e.TXIndex).Error()
-}
-
-func (e TransactionCommitError) Is(err error) bool {
-	_, ok := err.(*TransactionCommitError)
-	return ok
-}
-
-func NewTransactionCommitError(txIndex int, err error) *TransactionCommitError {
-	return &TransactionCommitError{
-		TXIndex: txIndex,
-		Err:     err,
-	}
-}
-
-func IsTransactionCommitError(err error) bool {
-	return errors.Is(err, &TransactionCommitError{})
-}
-
-type InsufficientFundError struct {
-	Asset string
-}
-
-func (e InsufficientFundError) Error() string {
-	return fmt.Sprintf("balance.insufficient.%s", e.Asset)
-}
-
-func (e InsufficientFundError) Is(err error) bool {
-	_, ok := err.(*InsufficientFundError)
-	return ok
-}
-
-func NewInsufficientFundError(asset string) *InsufficientFundError {
-	return &InsufficientFundError{
-		Asset: asset,
-	}
-}
-
-func IsInsufficientFundError(err error) bool {
-	return errors.Is(err, &InsufficientFundError{})
-}
-
 type ValidationError struct {
 	Msg string
 }
@@ -102,29 +50,6 @@ func NewConflictError(msg string) *ConflictError {
 
 func IsConflictError(err error) bool {
 	return errors.Is(err, &ConflictError{})
-}
-
-type LockError struct {
-	Err error
-}
-
-func (e LockError) Error() string {
-	return e.Err.Error()
-}
-
-func (e LockError) Is(err error) bool {
-	_, ok := err.(*LockError)
-	return ok
-}
-
-func IsLockError(err error) bool {
-	return errors.Is(err, &LockError{})
-}
-
-func NewLockError(err error) *LockError {
-	return &LockError{
-		Err: err,
-	}
 }
 
 type NotFoundError struct {
