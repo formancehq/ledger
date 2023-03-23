54c98b8420376a491351e3c4d34ee5f771733995
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-03-23 18:19:13 +0100

    fix: cache (concurrent read cause multiple db accesses) (#178)

--- pkg/ledger/cache/cache_test.go
+++ pkg/ledger/cache/cache_test.go
@@ -5,166 +5,124 @@ import (
 	"testing"
 
 	"github.com/formancehq/ledger/pkg/core"
-	"github.com/formancehq/ledger/pkg/ledgertesting"
-	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sync/errgroup"
 )
 
-func TestComputeAccountFromLogs(t *testing.T) {
-
-	driver := ledgertesting.StorageDriver(t)
-
-	require.NoError(t, driver.Initialize(context.Background()))
-
-	manager := NewManager(driver)
-	ledger := uuid.NewString()
-	cache, err := manager.ForLedger(context.Background(), ledger)
-	require.NoError(t, err)
-
-	ledgerStore, _, err := driver.GetLedgerStore(context.Background(), ledger, true)
-	require.NoError(t, err)
-
-	_, err = ledgerStore.Initialize(context.Background())
-	require.NoError(t, err)
-
-	require.NoError(t, ledgerStore.EnsureAccountExists(context.Background(), "world"))
-	require.NoError(t, ledgerStore.UpdateVolumes(context.Background(), core.AccountsAssetsVolumes{
-		"world": {
-			"USD/2": {
-				Input:  core.NewMonetaryInt(100),
-				Output: core.NewMonetaryInt(0),
-			},
-		},
-	}))
-	log := core.NewTransactionLog(core.Transaction{
-		TransactionData: core.TransactionData{
-			Postings: []core.Posting{{
-				Source:      "world",
-				Destination: "bank",
-				Amount:      core.NewMonetaryInt(10),
-				Asset:       "USD/2",
-			}},
-		},
-	}, nil)
-	require.NoError(t, ledgerStore.AppendLog(context.Background(), &log))
-
-	log2 := core.NewSetMetadataLog(core.Now(), core.SetMetadataLogPayload{
-		TargetType: core.MetaTargetTypeAccount,
-		TargetID:   "bank",
-		Metadata: core.Metadata{
-			"category": "gold",
-		},
-	})
-	require.NoError(t, ledgerStore.AppendLog(context.Background(), &log2))
-
-	account, err := cache.GetAccountWithVolumes(context.Background(), "bank")
-	require.NoError(t, err)
-	require.NotNil(t, account)
-
-	require.Equal(t, core.AccountWithVolumes{
-		Account: core.Account{
-			Address: "bank",
-			Metadata: core.Metadata{
-				"category": "gold",
-			},
-		},
-		Volumes: map[string]core.Volumes{
-			"USD/2": {
-				Input:  core.NewMonetaryInt(10),
-				Output: core.NewMonetaryInt(0),
-			},
-		},
-	}, *account)
-
+type mockCall struct {
+	address string
 }
 
-func TestRetrieveValueFromCache(t *testing.T) {
+type mockAccountComputer struct {
+	accounts []*core.AccountWithVolumes
+	calls    []mockCall
+}
 
-	driver := ledgertesting.StorageDriver(t)
+func (c *mockAccountComputer) ComputeAccount(ctx context.Context, address string) (*core.AccountWithVolumes, error) {
+	c.calls = append(c.calls, mockCall{
+		address: address,
+	})
+	for _, accountWithVolumes := range c.accounts {
+		if accountWithVolumes.Address == address {
+			return accountWithVolumes, nil
+		}
+	}
+	return nil, nil
+}
 
-	require.NoError(t, driver.Initialize(context.Background()))
+func TestParallelRead(t *testing.T) {
 
-	manager := NewManager(driver)
-	ledger := uuid.NewString()
-	cache, err := manager.ForLedger(context.Background(), ledger)
-	require.NoError(t, err)
-
-	ledgerStore, _, err := driver.GetLedgerStore(context.Background(), ledger, true)
-	require.NoError(t, err)
-
-	_, err = ledgerStore.Initialize(context.Background())
-	require.NoError(t, err)
-
-	require.NoError(t, ledgerStore.EnsureAccountExists(context.Background(), "world"))
-	account, err := cache.GetAccountWithVolumes(context.Background(), "world")
-	require.NoError(t, err)
-	require.Equal(t, core.AccountWithVolumes{
-		Account: core.Account{
-			Address:  "world",
-			Metadata: core.Metadata{},
+	mock := &mockAccountComputer{
+		accounts: []*core.AccountWithVolumes{
+			{
+				Account: core.Account{
+					Address:  "world",
+					Metadata: core.Metadata{},
+				},
+				Volumes: map[string]core.Volumes{
+					"USD/2": {
+						Input:  core.NewMonetaryInt(100),
+						Output: core.NewMonetaryInt(0),
+					},
+				},
+			},
+			{
+				Account: core.Account{
+					Address: "bank",
+					Metadata: core.Metadata{
+						"category": "gold",
+					},
+				},
+				Volumes: map[string]core.Volumes{
+					"USD/2": {
+						Input:  core.NewMonetaryInt(10),
+						Output: core.NewMonetaryInt(0),
+					},
+				},
+			},
 		},
-		Volumes: map[string]core.Volumes{},
-	}, *account)
+	}
+	cache := New(mock)
 
-	cache.UpdateVolumeWithTX(core.NewTransaction().WithPostings(
-		core.NewPosting("world", "bank", "USD", core.NewMonetaryInt(100)),
-	))
-	account, err = cache.GetAccountWithVolumes(context.Background(), "world")
-	require.NoError(t, err)
-	require.EqualValues(t, core.AccountWithVolumes{
-		Account: core.Account{
-			Address:  "world",
-			Metadata: core.Metadata{},
-		},
-		Volumes: map[string]core.Volumes{
-			"USD": core.NewEmptyVolumes().WithOutput(core.NewMonetaryInt(100)),
-		},
-	}, *account)
+	eg := errgroup.Group{}
+	for i := 0; i < 1000; i++ {
+		eg.Go(func() error {
+			account, err := cache.GetAccountWithVolumes(context.Background(), "bank")
+			require.NoError(t, err)
+			require.NotNil(t, account)
+			require.Equal(t, core.AccountWithVolumes{
+				Account: core.Account{
+					Address: "bank",
+					Metadata: core.Metadata{
+						"category": "gold",
+					},
+				},
+				Volumes: map[string]core.Volumes{
+					"USD/2": {
+						Input:  core.NewMonetaryInt(10),
+						Output: core.NewMonetaryInt(0),
+					},
+				},
+			}, *account)
+			return nil
+		})
+	}
+
+	require.NoError(t, eg.Wait())
+	require.Len(t, mock.calls, 1)
 }
 
 func TestUpdateVolumes(t *testing.T) {
 
-	driver := ledgertesting.StorageDriver(t)
-
-	require.NoError(t, driver.Initialize(context.Background()))
-
-	manager := NewManager(driver)
-	ledger := uuid.NewString()
-	cache, err := manager.ForLedger(context.Background(), ledger)
-	require.NoError(t, err)
-
-	ledgerStore, _, err := driver.GetLedgerStore(context.Background(), ledger, true)
-	require.NoError(t, err)
-
-	_, err = ledgerStore.Initialize(context.Background())
-	require.NoError(t, err)
-
-	require.NoError(t, ledgerStore.EnsureAccountExists(context.Background(), "world"))
-	worldAccount, err := cache.GetAccountWithVolumes(context.Background(), "world")
-	require.NoError(t, err)
-	require.Equal(t, core.AccountWithVolumes{
-		Account: core.Account{
-			Address:  "world",
-			Metadata: core.Metadata{},
+	mock := &mockAccountComputer{
+		accounts: []*core.AccountWithVolumes{
+			{
+				Account: core.Account{
+					Address: "world",
+				},
+				Volumes: map[string]core.Volumes{},
+			},
+			{
+				Account: core.Account{
+					Address: "bank",
+				},
+				Volumes: map[string]core.Volumes{},
+			},
 		},
-		Volumes: map[string]core.Volumes{},
-	}, *worldAccount)
+	}
+	cache := New(mock)
 
-	require.NoError(t, ledgerStore.EnsureAccountExists(context.Background(), "bank"))
-	bankAccount, err := cache.GetAccountWithVolumes(context.Background(), "bank")
+	// Force load accounts
+	_, err := cache.GetAccountWithVolumes(context.Background(), "world")
 	require.NoError(t, err)
-	require.Equal(t, core.AccountWithVolumes{
-		Account: core.Account{
-			Address:  "bank",
-			Metadata: core.Metadata{},
-		},
-		Volumes: map[string]core.Volumes{},
-	}, *bankAccount)
+
+	_, err = cache.GetAccountWithVolumes(context.Background(), "bank")
+	require.NoError(t, err)
 
 	cache.UpdateVolumeWithTX(core.NewTransaction().WithPostings(
 		core.NewPosting("world", "bank", "USD", core.NewMonetaryInt(100)),
 	))
-	worldAccount, err = cache.GetAccountWithVolumes(context.Background(), "world")
+	worldAccount, err := cache.GetAccountWithVolumes(context.Background(), "world")
 	require.NoError(t, err)
 	require.EqualValues(t, core.AccountWithVolumes{
 		Account: core.Account{
