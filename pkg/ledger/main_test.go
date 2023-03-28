f34092d06c67aec5c3bf18e604fd058483db9690
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-03-28 11:59:34 +0200

    feat: centralize ledger lifecycle inside resolver. (#184)

--- pkg/ledger/main_test.go
+++ pkg/ledger/main_test.go
@@ -5,11 +5,8 @@ import (
 	"os"
 	"testing"
 
-	"github.com/formancehq/ledger/pkg/ledger/cache"
 	"github.com/formancehq/ledger/pkg/ledger/lock"
-	"github.com/formancehq/ledger/pkg/ledger/numscript"
 	"github.com/formancehq/ledger/pkg/ledger/query"
-	"github.com/formancehq/ledger/pkg/ledger/runner"
 	"github.com/formancehq/ledger/pkg/ledgertesting"
 	"github.com/formancehq/stack/libs/go-libs/logging"
 	"github.com/formancehq/stack/libs/go-libs/pgtesting"
@@ -29,38 +26,28 @@ func TestMain(t *testing.M) {
 	os.Exit(code)
 }
 
-func runOnLedger(t interface {
-	pgtesting.TestingT
-	Parallel()
-}, f func(l *Ledger)) {
-
-	t.Parallel()
-
+func newResolver(t interface{ pgtesting.TestingT }) *Resolver {
 	storageDriver := ledgertesting.StorageDriver(t)
 	require.NoError(t, storageDriver.Initialize(context.Background()))
-
-	name := uuid.New()
-	store, _, err := storageDriver.GetLedgerStore(context.Background(), name, true)
-	require.NoError(t, err)
-
-	_, err = store.Initialize(context.Background())
-	require.NoError(t, err)
-
-	cacheManager := cache.NewManager(storageDriver)
-	ledgerCache, err := cacheManager.ForLedger(context.Background(), name)
-	require.NoError(t, err)
-
-	compiler := numscript.NewCompiler()
-
-	runner, err := runner.New(store, lock.NewInMemory(), ledgerCache, compiler, false)
-	require.NoError(t, err)
 
 	queryWorker := query.NewWorker(query.DefaultWorkerConfig, storageDriver, query.NewNoOpMonitor())
 	go func() {
 		require.NoError(t, queryWorker.Run(context.Background()))
 	}()
 
-	l := New(store, ledgerCache, runner, lock.NewInMemory(), queryWorker)
+	return NewResolver(storageDriver, lock.NewInMemory(), queryWorker, false)
+}
+
+func runOnLedger(t interface {
+	pgtesting.TestingT
+	Parallel()
+}, f func(l *Ledger)) {
+	t.Parallel()
+
+	ledgerName := uuid.New()
+	resolver := newResolver(t)
+	l, err := resolver.GetLedger(context.Background(), ledgerName)
+	require.NoError(t, err)
 	defer l.Close(context.Background())
 
 	f(l)
