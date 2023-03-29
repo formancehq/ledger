55a69bb271f27a61a83a65819e11fc53fd198a18
Antoine Gelloz <antoine.gelloz@me.com>
2023-03-29 15:53:11 +0200

    fix: Reverse transaction (#189)

--- pkg/core/transaction_test.go
+++ pkg/core/transaction_test.go
@@ -3,51 +3,136 @@ package core
 import (
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
 )
 
 func TestReverseTransaction(t *testing.T) {
-	tx := &ExpandedTransaction{
-		Transaction: Transaction{
-			TransactionData: TransactionData{
-				Postings: Postings{
-					{
-						Source:      "world",
-						Destination: "users:001",
-						Amount:      NewMonetaryInt(100),
-						Asset:       "COIN",
+	t.Run("1 posting", func(t *testing.T) {
+		tx := &ExpandedTransaction{
+			Transaction: Transaction{
+				TransactionData: TransactionData{
+					Postings: Postings{
+						{
+							Source:      "world",
+							Destination: "users:001",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
 					},
-					{
-						Source:      "users:001",
-						Destination: "payments:001",
-						Amount:      NewMonetaryInt(100),
-						Asset:       "COIN",
-					},
+					Reference: "foo",
 				},
-				Reference: "foo",
 			},
-		},
-	}
+		}
 
-	expected := TransactionData{
-		Postings: Postings{
-			{
-				Source:      "payments:001",
-				Destination: "users:001",
-				Amount:      NewMonetaryInt(100),
-				Asset:       "COIN",
+		expected := TransactionData{
+			Postings: Postings{
+				{
+					Source:      "users:001",
+					Destination: "world",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
 			},
-			{
-				Source:      "users:001",
-				Destination: "world",
-				Amount:      NewMonetaryInt(100),
-				Asset:       "COIN",
-			},
-		},
-		Reference: "revert_foo",
-	}
+			Reference: "revert_foo",
+		}
+		require.Equal(t, expected, tx.Reverse())
+	})
 
-	if diff := cmp.Diff(expected, tx.Reverse()); diff != "" {
-		t.Errorf("Reverse() mismatch (-want +got):\n%s", diff)
-	}
+	t.Run("2 postings", func(t *testing.T) {
+		tx := &ExpandedTransaction{
+			Transaction: Transaction{
+				TransactionData: TransactionData{
+					Postings: Postings{
+						{
+							Source:      "world",
+							Destination: "users:001",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
+						{
+							Source:      "users:001",
+							Destination: "payments:001",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
+					},
+					Reference: "foo",
+				},
+			},
+		}
+
+		expected := TransactionData{
+			Postings: Postings{
+				{
+					Source:      "payments:001",
+					Destination: "users:001",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
+				{
+					Source:      "users:001",
+					Destination: "world",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
+			},
+			Reference: "revert_foo",
+		}
+		require.Equal(t, expected, tx.Reverse())
+	})
+
+	t.Run("3 postings", func(t *testing.T) {
+		tx := &ExpandedTransaction{
+			Transaction: Transaction{
+				TransactionData: TransactionData{
+					Postings: Postings{
+						{
+							Source:      "world",
+							Destination: "users:001",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
+						{
+							Source:      "users:001",
+							Destination: "payments:001",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
+						{
+							Source:      "payments:001",
+							Destination: "alice",
+							Amount:      NewMonetaryInt(100),
+							Asset:       "COIN",
+						},
+					},
+					Reference: "foo",
+				},
+			},
+		}
+
+		expected := TransactionData{
+			Postings: Postings{
+				{
+					Source:      "alice",
+					Destination: "payments:001",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
+				{
+					Source:      "payments:001",
+					Destination: "users:001",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
+				{
+					Source:      "users:001",
+					Destination: "world",
+					Amount:      NewMonetaryInt(100),
+					Asset:       "COIN",
+				},
+			},
+			Reference: "revert_foo",
+		}
+		require.Equal(t, expected, tx.Reverse())
+	})
 }
