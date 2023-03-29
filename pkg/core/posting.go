0ec58e6ca9bf410ffc685f27be188be30d4226e5
Antoine Gelloz <antoine.gelloz@me.com>
2023-03-29 14:37:03 +0200

    fix: first

--- pkg/core/posting.go
+++ pkg/core/posting.go
@@ -18,15 +18,12 @@ type Posting struct {
 type Postings []Posting
 
 func (ps Postings) Reverse() {
-	if len(ps) == 1 {
-		ps[0].Source, ps[0].Destination = ps[0].Destination, ps[0].Source
-		return
-	}
-	for i := len(ps)/2 - 1; i >= 0; i-- {
-		opp := len(ps) - 1 - i
-		ps[i], ps[opp] = ps[opp], ps[i]
+	for i := range ps {
 		ps[i].Source, ps[i].Destination = ps[i].Destination, ps[i].Source
-		ps[opp].Source, ps[opp].Destination = ps[opp].Destination, ps[opp].Source
+	}
+
+	for i := 0; i < len(ps)/2; i++ {
+		ps[i], ps[len(ps)-i-1] = ps[len(ps)-i-1], ps[i]
 	}
 }
 
