d0a446214e42b9b236897ab7bd714164effa6a6b
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-04-12 11:56:27 +0200

    feat: merge all migrations to one (#225)

--- pkg/storage/sqlstorage/ledger/migrates/14-update-timestamp-column-type/postgres.sql
+++
@@ -1,8 +0,0 @@
---statement
-ALTER TABLE "VAR_LEDGER_NAME".transactions ADD COLUMN timestamp_holder timestamptz NULL;
---statement
-UPDATE "VAR_LEDGER_NAME".transactions SET timestamp_holder = timestamp::TIMESTAMP;
---statement
-ALTER TABLE "VAR_LEDGER_NAME".transactions ALTER COLUMN timestamp TYPE timestamptz USING timestamp_holder;
---statement
-ALTER TABLE "VAR_LEDGER_NAME".transactions DROP COLUMN timestamp_holder;
