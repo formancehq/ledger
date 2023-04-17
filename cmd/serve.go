ecc80f76763a45a271d9fe702aabc78ecc23a0d7
Paul Nicolas <paul.nicolas.go@gmail.com>
2023-04-17 14:29:31 +0200

    feat: add ballast package (#243)

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/formancehq/ledger/pkg/ledger/cache"
+	"github.com/formancehq/stack/libs/go-libs/ballast"
 	"github.com/formancehq/stack/libs/go-libs/httpserver"
 	app "github.com/formancehq/stack/libs/go-libs/service"
 	"github.com/go-chi/chi/v5"
@@ -14,6 +15,8 @@ const (
 	cacheEvictionPeriodFlag  = "cache-eviction-period"
 	cacheEvictionRetainDelay = "cache-eviction-retain-delay"
 	queryLimitReadLogsFlag   = "query-limit-read-logs"
+
+	ballastSizeInBytesFlag = "ballast-size"
 )
 
 func NewServe() *cobra.Command {
@@ -22,6 +25,7 @@ func NewServe() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return app.New(cmd.OutOrStdout(), resolveOptions(
 				viper.GetViper(),
+				ballast.Module(viper.GetUint(ballastSizeInBytesFlag)),
 				fx.Invoke(func(lc fx.Lifecycle, h chi.Router) {
 					lc.Append(httpserver.NewHook(viper.GetString(bindFlag), h))
 				}),
@@ -31,5 +35,6 @@ func NewServe() *cobra.Command {
 	cmd.Flags().Duration(cacheEvictionPeriodFlag, cache.DefaultEvictionPeriod, "Cache eviction period")
 	cmd.Flags().Duration(cacheEvictionRetainDelay, cache.DefaultRetainDelay, "Cache retain delay")
 	cmd.Flags().Int(queryLimitReadLogsFlag, 10000, "Query limit read logs")
+	cmd.Flags().Uint(ballastSizeInBytesFlag, 0, "Ballast size in bytes, default to 0")
 	return cmd
 }
