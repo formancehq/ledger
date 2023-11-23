95f93bda6d48eb750250afc9fa2d1fc6602f4448
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2023-11-23 17:19:47 +0100

    feat: various bench improvements (#887)

--- internal/storage/ledgerstore/store_benchmarks_test.go
+++ internal/storage/ledgerstore/store_benchmarks_test.go
@@ -17,77 +17,112 @@ import (
 )
 
 var nbTransactions = flag.Int("transactions", 10000, "number of transactions to create")
+var batch = flag.Int("batch", 1000, "logs batching")
+
+type scenarioInfo struct {
+	nbAccounts int
+}
+
+type scenario struct {
+	name  string
+	setup func(ctx context.Context, b *testing.B, store *Store) *scenarioInfo
+}
+
+var scenarios = []scenario{
+	{
+		name: "nominal",
+		setup: func(ctx context.Context, b *testing.B, store *Store) *scenarioInfo {
+			var lastLog *ledger.ChainedLog
+			for i := 0; i < *nbTransactions/(*batch); i++ {
+				logs := make([]*ledger.ChainedLog, 0)
+				appendLog := func(log *ledger.Log) {
+					chainedLog := log.ChainLog(lastLog)
+					logs = append(logs, chainedLog)
+					lastLog = chainedLog
+				}
+				for j := 0; j < (*batch); j += 2 {
+					provision := big.NewInt(10000)
+					itemPrice := provision.Div(provision, big.NewInt(2))
+					fees := itemPrice.Div(itemPrice, big.NewInt(100)) // 1%
+
+					appendLog(ledger.NewTransactionLog(
+						ledger.NewTransaction().WithPostings(ledger.NewPosting(
+							"world", fmt.Sprintf("player:%d", j/2), "USD/2", provision,
+						)).WithID(big.NewInt(int64(i*(*batch)+j))),
+						map[string]metadata.Metadata{},
+					))
+					appendLog(ledger.NewTransactionLog(
+						ledger.NewTransaction().WithPostings(
+							ledger.NewPosting(fmt.Sprintf("player:%d", j/2), "seller", "USD/2", itemPrice),
+							ledger.NewPosting("seller", "fees", "USD/2", fees),
+						).WithID(big.NewInt(int64(i*(*batch)+j+1))),
+						map[string]metadata.Metadata{},
+					))
+					status := "pending"
+					if j%8 == 0 {
+						status = "terminated"
+					}
+					appendLog(ledger.NewSetMetadataLog(ledger.Now(), ledger.SetMetadataLogPayload{
+						TargetType: ledger.MetaTargetTypeTransaction,
+						TargetID:   big.NewInt(int64(i*(*batch) + j + 1)),
+						Metadata: map[string]string{
+							"status": status,
+						},
+					}))
+				}
+				require.NoError(b, store.InsertLogs(ctx, logs...))
+			}
+
+			nbAccounts := *batch / 2
+
+			for i := 0; i < nbAccounts; i++ {
+				lastLog = ledger.NewSetMetadataLog(ledger.Now(), ledger.SetMetadataLogPayload{
+					TargetType: ledger.MetaTargetTypeAccount,
+					TargetID:   fmt.Sprintf("player:%d", i),
+					Metadata: map[string]string{
+						"level": fmt.Sprint(i % 4),
+					},
+				}).ChainLog(lastLog)
+				require.NoError(b, store.InsertLogs(ctx, lastLog))
+			}
+
+			return &scenarioInfo{
+				nbAccounts: nbAccounts,
+			}
+		},
+	},
+}
 
 func BenchmarkList(b *testing.B) {
-	const batchSize = 1000
 
 	ctx := logging.TestingContext()
 	hooks := make([]bun.QueryHook, 0)
 	if testing.Verbose() {
 		hooks = append(hooks, bunexplain.NewExplainHook())
 	}
-	store := newLedgerStore(b, hooks...)
 
-	var lastLog *ledger.ChainedLog
-	for i := 0; i < *nbTransactions/batchSize; i++ {
-		batch := make([]*ledger.ChainedLog, 0)
-		appendLog := func(log *ledger.Log) {
-			chainedLog := log.ChainLog(lastLog)
-			batch = append(batch, chainedLog)
-			lastLog = chainedLog
-		}
-		for j := 0; j < batchSize; j += 2 {
-			provision := big.NewInt(10000)
-			itemPrice := provision.Div(provision, big.NewInt(2))
-			fees := itemPrice.Div(itemPrice, big.NewInt(100)) // 1%
+	for _, scenario := range scenarios {
+		b.Run(scenario.name, func(b *testing.B) {
+			store := newLedgerStore(b, hooks...)
+			info := scenario.setup(ctx, b, store)
 
-			appendLog(ledger.NewTransactionLog(
-				ledger.NewTransaction().WithPostings(ledger.NewPosting(
-					"world", fmt.Sprintf("player:%d", j/2), "USD/2", provision,
-				)).WithID(big.NewInt(int64(i*batchSize+j))),
-				map[string]metadata.Metadata{},
-			))
-			appendLog(ledger.NewTransactionLog(
-				ledger.NewTransaction().WithPostings(
-					ledger.NewPosting(fmt.Sprintf("player:%d", j/2), "seller", "USD/2", itemPrice),
-					ledger.NewPosting("seller", "fees", "USD/2", fees),
-				).WithID(big.NewInt(int64(i*batchSize+j+1))),
-				map[string]metadata.Metadata{},
-			))
-			status := "pending"
-			if j%8 == 0 {
-				status = "terminated"
-			}
-			appendLog(ledger.NewSetMetadataLog(ledger.Now(), ledger.SetMetadataLogPayload{
-				TargetType: ledger.MetaTargetTypeTransaction,
-				TargetID:   big.NewInt(int64(i*batchSize + j + 1)),
-				Metadata: map[string]string{
-					"status": status,
-				},
-			}))
-		}
-		require.NoError(b, store.InsertLogs(ctx, batch...))
+			_, err := store.db.Exec("VACUUM ANALYZE")
+			require.NoError(b, err)
+
+			b.Run("transactions", func(b *testing.B) {
+				benchmarksReadTransactions(b, ctx, store, info)
+			})
+			b.Run("accounts", func(b *testing.B) {
+				benchmarksReadAccounts(b, ctx, store)
+			})
+			b.Run("aggregates", func(b *testing.B) {
+				benchmarksGetAggregatedBalances(b, ctx, store)
+			})
+		})
 	}
-
-	nbAccounts := batchSize / 2
-
-	for i := 0; i < nbAccounts; i++ {
-		lastLog = ledger.NewSetMetadataLog(ledger.Now(), ledger.SetMetadataLogPayload{
-			TargetType: ledger.MetaTargetTypeAccount,
-			TargetID:   fmt.Sprintf("player:%d", i),
-			Metadata: map[string]string{
-				"level": fmt.Sprint(i % 4),
-			},
-		}).ChainLog(lastLog)
-		require.NoError(b, store.InsertLogs(ctx, lastLog))
-	}
-
-	benchmarksReadTransactions(b, ctx, store, *nbTransactions, nbAccounts)
-	benchmarksReadAccounts(b, ctx, store, nbAccounts)
-	benchmarksGetAggregatedBalances(b, ctx, store, nbAccounts)
 }
 
-func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store, nbTransactions, nbAccounts int) {
+func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store, info *scenarioInfo) {
 	type testCase struct {
 		name                   string
 		query                  query.Builder
@@ -98,15 +133,15 @@ func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store,
 
 	testCases := []testCase{
 		{
-			name: "with no query",
+			name: "no query",
 		},
 		{
 			name:  "using an exact address",
-			query: query.Match("account", fmt.Sprintf("player:%d", nbAccounts-1)), // Last inserted account
+			query: query.Match("account", fmt.Sprintf("player:%d", info.nbAccounts-1)), // Last inserted account
 		},
 		{
 			name:  "using an address segment",
-			query: query.Match("account", fmt.Sprintf(":%d", nbAccounts-1)),
+			query: query.Match("account", fmt.Sprintf(":%d", info.nbAccounts-1)),
 		},
 		{
 			name:  "using a metadata metadata",
@@ -114,7 +149,7 @@ func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store,
 		},
 		{
 			name:               "using non existent account by exact address",
-			query:              query.Match("account", fmt.Sprintf("player:%d", nbAccounts)),
+			query:              query.Match("account", fmt.Sprintf("player:%d", info)),
 			allowEmptyResponse: true,
 		},
 		{
@@ -134,7 +169,7 @@ func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store,
 
 	for _, t := range testCases {
 		t := t
-		b.Run("listing transactions "+t.name, func(b *testing.B) {
+		b.Run(t.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				q := NewGetTransactionsQuery(PaginatedQueryOptions[PITFilterWithVolumes]{
 					PageSize:     10,
@@ -156,7 +191,7 @@ func benchmarksReadTransactions(b *testing.B, ctx context.Context, store *Store,
 	}
 }
 
-func benchmarksReadAccounts(b *testing.B, ctx context.Context, store *Store, nbAccounts int) {
+func benchmarksReadAccounts(b *testing.B, ctx context.Context, store *Store) {
 	type testCase struct {
 		name                                  string
 		query                                 query.Builder
@@ -188,7 +223,7 @@ func benchmarksReadAccounts(b *testing.B, ctx context.Context, store *Store, nbA
 
 	for _, t := range testCases {
 		t := t
-		b.Run("listing accounts "+t.name, func(b *testing.B) {
+		b.Run(t.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				q := NewGetAccountsQuery(PaginatedQueryOptions[PITFilterWithVolumes]{
 					PageSize:     10,
@@ -210,7 +245,7 @@ func benchmarksReadAccounts(b *testing.B, ctx context.Context, store *Store, nbA
 	}
 }
 
-func benchmarksGetAggregatedBalances(b *testing.B, ctx context.Context, store *Store, nbAccounts int) {
+func benchmarksGetAggregatedBalances(b *testing.B, ctx context.Context, store *Store) {
 	type testCase struct {
 		name               string
 		query              query.Builder
@@ -237,7 +272,7 @@ func benchmarksGetAggregatedBalances(b *testing.B, ctx context.Context, store *S
 
 	for _, t := range testCases {
 		t := t
-		b.Run("aggregating balance "+t.name, func(b *testing.B) {
+		b.Run(t.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
 				ret, err := store.GetAggregatedBalances(ctx, NewGetAggregatedBalancesQuery(PaginatedQueryOptions[PITFilter]{
 					PageSize:     10,
