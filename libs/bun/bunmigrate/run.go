7274df444e1934aa5d226a1a7937b08b61f939de
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2024-02-15 12:49:08 +0100

    feat(operator-utils): add operator utils (#1243)

--- libs/bun/bunmigrate/run.go
+++ libs/bun/bunmigrate/run.go
@@ -17,12 +17,29 @@ import (
 	"io"
 )
 
-func ensureDatabaseExists(ctx context.Context, connectionOptions bunconnect.ConnectionOptions) error {
+func isDatabaseExists(ctx context.Context, db *bun.DB, name string) (bool, error) {
+	row := db.QueryRowContext(ctx, `SELECT datname FROM pg_database WHERE datname = ?`, name)
+	if row.Err() != nil {
+		return false, errors.Wrap(row.Err(), "checking database list")
+	}
+
+	if err := row.Scan(pointer.For("")); err != nil {
+		if !errors.Is(err, sql.ErrNoRows) {
+			return false, errors.Wrap(err, "scanning database row")
+		}
+
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func onPostgresDB(ctx context.Context, connectionOptions bunconnect.ConnectionOptions, callback func(db *bun.DB) error) error {
 	url, err := dburl.Parse(connectionOptions.DatabaseSourceName)
 	if err != nil {
-		return err
+		return errors.Wrapf(err, "parsing dsn: %s", connectionOptions.DatabaseSourceName)
 	}
-	originalPath := url.Path
+
 	url.Path = "postgres" // notes(gfyrag): default "postgres" database (most of the time?)
 	connectionOptions.DatabaseSourceName = url.String()
 
@@ -37,29 +54,61 @@ func ensureDatabaseExists(ctx context.Context, connectionOptions bunconnect.Conn
 		}
 	}()
 
-	row := db.QueryRowContext(ctx, `SELECT datname FROM pg_database WHERE datname = ?`, originalPath[1:])
-	if row.Err() != nil {
-		return row.Err()
-	}
+	return callback(db)
+}
 
-	if err := row.Scan(pointer.For("")); err != nil {
-		if !errors.Is(err, sql.ErrNoRows) {
-			return err
-		}
+func EnsureDatabaseNotExists(ctx context.Context, connectionOptions bunconnect.ConnectionOptions) error {
+	return onPostgresDB(ctx, connectionOptions, func(db *bun.DB) error {
 
-		_, err = db.ExecContext(ctx, fmt.Sprintf(`CREATE DATABASE "%s"`, originalPath[1:]))
+		url, err := dburl.Parse(connectionOptions.DatabaseSourceName)
 		if err != nil {
-			return err
+			return errors.Wrapf(err, "parsing dsn: %s", connectionOptions.DatabaseSourceName)
 		}
-	}
 
-	return nil
+		databaseExists, err := isDatabaseExists(ctx, db, url.Path[1:])
+		if err != nil {
+			return errors.Wrap(err, "checking if database exists")
+		}
+
+		if databaseExists {
+			_, err = db.ExecContext(ctx, fmt.Sprintf(`DROP DATABASE "%s"`, url.Path[1:]))
+			if err != nil {
+				return errors.Wrap(err, "dropping database")
+			}
+		}
+
+		return nil
+	})
+}
+
+func EnsureDatabaseExists(ctx context.Context, connectionOptions bunconnect.ConnectionOptions) error {
+	return onPostgresDB(ctx, connectionOptions, func(db *bun.DB) error {
+
+		url, err := dburl.Parse(connectionOptions.DatabaseSourceName)
+		if err != nil {
+			return errors.Wrapf(err, "parsing dsn: %s", connectionOptions.DatabaseSourceName)
+		}
+
+		databaseExists, err := isDatabaseExists(ctx, db, url.Path[1:])
+		if err != nil {
+			return errors.Wrap(err, "checking if database exists")
+		}
+
+		if !databaseExists {
+			_, err = db.ExecContext(ctx, fmt.Sprintf(`CREATE DATABASE "%s"`, url.Path[1:]))
+			if err != nil {
+				return errors.Wrap(err, "creating database")
+			}
+		}
+
+		return nil
+	})
 }
 
 func run(ctx context.Context, output io.Writer, args []string, connectionOptions *bunconnect.ConnectionOptions,
 	executor func(args []string, db *bun.DB) error) error {
 
-	if err := ensureDatabaseExists(ctx, *connectionOptions); err != nil {
+	if err := EnsureDatabaseExists(ctx, *connectionOptions); err != nil {
 		return err
 	}
 
