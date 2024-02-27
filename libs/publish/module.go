b7d5e282bb466d0ba1d3bc61b691dbbc375b672c
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2024-02-27 14:45:19 +0100

    feat(orchestration): add some events (#1286)

--- libs/publish/module.go
+++ libs/publish/module.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ThreeDotsLabs/watermill/message"
 	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
 	"go.uber.org/fx"
+	"sync"
 )
 
 func newGoChannel() *gochannel.GoChannel {
@@ -93,5 +94,51 @@ func NewTopicMapperPublisherDecorator(publisher message.Publisher, topics map[st
 	return &topicMapperPublisherDecorator{
 		Publisher: publisher,
 		topics:    topics,
+	}
+}
+
+type noOpPublisher struct {
+}
+
+func (n noOpPublisher) Publish(topic string, messages ...*message.Message) error {
+	return nil
+}
+
+func (n noOpPublisher) Close() error {
+	return nil
+}
+
+var NoOpPublisher message.Publisher = &noOpPublisher{}
+
+type memoryPublisher struct {
+	sync.Mutex
+	messages map[string][]*message.Message
+}
+
+func (m *memoryPublisher) Publish(topic string, messages ...*message.Message) error {
+	m.Lock()
+	defer m.Unlock()
+
+	m.messages[topic] = append(m.messages[topic], messages...)
+	return nil
+}
+
+func (m *memoryPublisher) Close() error {
+	m.Lock()
+	defer m.Unlock()
+
+	m.messages = map[string][]*message.Message{}
+	return nil
+}
+
+func (m *memoryPublisher) AllMessages() map[string][]*message.Message {
+	return m.messages
+}
+
+var _ message.Publisher = (*memoryPublisher)(nil)
+
+func InMemory() *memoryPublisher {
+	return &memoryPublisher{
+		messages: map[string][]*message.Message{},
 	}
 }
