d084b9277be355be73b0a20b2c639104a4879dd4
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-01 12:47:47 +0200

    feat: compute hash at core level

--- internal/storage/bucket/migrations/11-stateless.sql
+++ internal/storage/bucket/migrations/11-stateless.sql
@@ -231,47 +231,6 @@ begin
 end;
 $$;
 
-create function "{{.Bucket}}".set_log_hash()
-	returns trigger
-	security definer
-	language plpgsql
-as
-$$
-declare
-	previousHash bytea;
-	marshalledAsJSON varchar;
-begin
-	select hash into previousHash
-	from "{{.Bucket}}".logs
-	where ledger = new.ledger
-	order by seq desc
-	limit 1;
-
-	-- select only fields participating in the hash on the backend and format json representation the same way
-	select public.json_compact(json_build_object(
-		'type', new.type,
-		'data', new.data,
-		'date', to_json(new.date::timestamp)#>>'{}' || 'Z',
-		'idempotencyKey', coalesce(new.idempotency_key, ''),
-		'id', 0,
-		'hash', null
-	)) into marshalledAsJSON;
-
-	new.hash = (
-		select public.digest(
-			case
-				when previousHash is null
-					then marshalledAsJSON::bytea
-					else '"' || encode(previousHash::bytea, 'base64')::bytea || E'"\n' || convert_to(marshalledAsJSON, 'LATIN1')::bytea
-			end || E'\n', 'sha256'::text
-        )
-    );
-
-	return new;
-end;
-$$;
-
-
 create or replace function "{{.Bucket}}".update_transaction_metadata_history() returns trigger
 	security definer
 	language plpgsql
