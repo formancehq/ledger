d962308de98025abc830135fae1652e9652fac0a
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-04 08:43:06 +0200

    feat: add report file on benchmarks

--- test/performance/report_test.go
+++ test/performance/report_test.go
@@ -3,6 +3,7 @@
 package performance_test
 
 import (
+	"encoding/json"
 	"sync"
 	"sync/atomic"
 
@@ -10,36 +11,57 @@ import (
 )
 
 type Report struct {
-	mu sync.Mutex
+	mu *sync.Mutex
 
-	features      map[string]string
+	Features map[string]string
 
-	startOfBench time.Time
-	endOfBench   time.Time
+	Start time.Time
+	End   time.Time
 
-	totalDuration atomic.Int64
-	transactionsCount int
+	TotalLatency      *atomic.Int64
+	TransactionsCount int
+	Name              string
+}
+
+func (r *Report) MarshalJSON() ([]byte, error) {
+	type Aux Report
+	return json.Marshal(struct {
+		Aux
+		TotalLatency int64
+	}{
+		Aux:          Aux(*r),
+		TotalLatency: r.TotalLatency.Load(),
+	})
 }
 
 func (r *Report) TPS() float64 {
-	return (float64(time.Duration(r.transactionsCount)) / float64(r.endOfBench.Sub(r.startOfBench))) * float64(time.Second)
+	return (float64(time.Duration(r.TransactionsCount)) / float64(r.End.Sub(r.Start))) * float64(time.Second)
 }
 
 func (r *Report) AverageDuration() time.Duration {
-	return time.Duration(r.totalDuration.Load()) * time.Millisecond / time.Duration(r.transactionsCount)
+	return time.Duration(r.TotalLatency.Load()) * time.Millisecond / time.Duration(r.TransactionsCount)
 }
 
 func (r *Report) registerTransactionLatency(latency time.Duration) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
-	r.transactionsCount++
-	r.totalDuration.Add(latency.Milliseconds())
+	r.TransactionsCount++
+	r.TotalLatency.Add(latency.Milliseconds())
 }
 
-func newReport(features map[string]string) *Report {
-	return &Report{
-		startOfBench: time.Now(),
-		features: features,
+func (r *Report) reset() {
+	r.TotalLatency = &atomic.Int64{}
+	r.TransactionsCount = 0
+	r.Start = time.Now()
+}
+
+func newReport(features map[string]string, name string) *Report {
+	ret := &Report{
+		Name: name,
+		Features:     features,
+		mu:           &sync.Mutex{},
 	}
+	ret.reset()
+	return ret
 }
