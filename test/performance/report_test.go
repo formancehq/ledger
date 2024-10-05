f2dc50c24470d19879e37e5e8fe5a36562a22abf
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-05 22:58:28 +0200

    chore: renaming

--- test/performance/report_test.go
+++ test/performance/report_test.go
@@ -3,13 +3,23 @@
 package performance_test
 
 import (
-	"encoding/json"
 	"github.com/formancehq/go-libs/time"
 	"github.com/jamiealquiza/tachymeter"
 	"sync"
 )
 
-type Report struct {
+type Result struct {
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+
+	Metrics *tachymeter.Metrics `json:"metrics"`
+
+	Name              string `json:"name"`
+	Configuration     configuration `json:"configuration"`
+	TPS float64 `json:"tps"`
+}
+
+type report struct {
 	mu *sync.Mutex
 
 	Start time.Time
@@ -17,50 +27,40 @@ type Report struct {
 
 	Tachymeter *tachymeter.Tachymeter
 
-	Name              string
-	Configuration     configuration
+	Scenario      string
+	Configuration configuration
 }
 
-func (r Report) MarshalJSON() ([]byte, error) {
-	type view struct {
-		Start time.Time `json:"start"`
-		End   time.Time `json:"end"`
-
-		Metrics *tachymeter.Metrics `json:"metrics"`
-
-		Name              string `json:"name"`
-		Configuration     configuration `json:"configuration"`
-		TPS float64 `json:"tps"`
-	}
-	return json.Marshal(view{
+func (r *report) GetResult() Result {
+	return Result{
 		Start:         r.Start,
 		End:           r.End,
 		Metrics:       r.Tachymeter.Calc(),
-		Name:          r.Name,
+		Name:          r.Scenario,
 		Configuration: r.Configuration,
 		TPS: r.TPS(),
-	})
+	}
 }
 
-func (r *Report) TPS() float64 {
+func (r *report) TPS() float64 {
 	return (float64(time.Duration(r.Tachymeter.Count)) / float64(r.End.Sub(r.Start))) * float64(time.Second)
 }
 
-func (r *Report) registerTransactionLatency(latency time.Duration) {
+func (r *report) registerTransactionLatency(latency time.Duration) {
 	r.mu.Lock()
 	defer r.mu.Unlock()
 
 	r.Tachymeter.AddTime(latency)
 }
 
-func (r *Report) reset() {
+func (r *report) reset() {
 	r.Start = time.Now()
 	r.Tachymeter.Reset()
 }
 
-func newReport(configuration configuration, name string) Report {
-	ret := Report{
-		Name:          name,
+func newReport(configuration configuration, scenario string) report {
+	ret := report{
+		Scenario:      scenario,
 		Configuration: configuration,
 		mu:            &sync.Mutex{},
 		Tachymeter: tachymeter.New(&tachymeter.Config{
