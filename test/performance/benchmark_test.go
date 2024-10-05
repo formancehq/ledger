39c609ec4e2708a99722738b511bb7fe0161c487
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-05 13:37:04 +0200

    feat: refine benchmarks

--- test/performance/benchmark_test.go
+++ test/performance/benchmark_test.go
@@ -17,8 +17,8 @@ import (
 )
 
 type Benchmark struct {
-	EnvFactories map[string]EnvFactory
-	Scripts      map[string]func(int) (string, map[string]string)
+	EnvFactory EnvFactory
+	Scripts    map[string]func(int) (string, map[string]string)
 
 	reports map[string]map[string]*Report
 	b       *testing.B
@@ -26,63 +26,61 @@ type Benchmark struct {
 
 func (benchmark *Benchmark) Run(ctx context.Context) map[string][]Report {
 	reports := make(map[string][]Report, 0)
-	for envName, envFactory := range benchmark.EnvFactories {
-		scriptsKeys := Keys(benchmark.Scripts)
-		sort.Strings(scriptsKeys)
+	scriptsKeys := Keys(benchmark.Scripts)
+	sort.Strings(scriptsKeys)
 
-		for _, scriptName := range scriptsKeys {
-			for _, configuration := range buildAllPossibleConfigurations() {
+	for _, scriptName := range scriptsKeys {
+		for _, configuration := range buildAllPossibleConfigurations() {
 
-				testName := fmt.Sprintf("%s/%s/%s", envName, scriptName, configuration)
+			testName := fmt.Sprintf("%s/%s", scriptName, configuration)
 
-				ledgerConfiguration := ledger.Configuration{
-					Features: configuration.FeatureSet,
-					Bucket:   uuid.NewString()[:8],
+			ledgerConfiguration := ledger.Configuration{
+				Features: configuration.FeatureSet,
+				Bucket:   uuid.NewString()[:8],
+			}
+			ledgerConfiguration.SetDefaults()
+			report := newReport(configuration, scriptName)
+
+			benchmark.b.Run(testName, func(b *testing.B) {
+				report.reset()
+				l := ledger.Ledger{
+					Configuration: ledgerConfiguration,
+					Name:          uuid.NewString()[:8],
 				}
-				ledgerConfiguration.SetDefaults()
-				report := newReport(configuration, scriptName)
 
-				benchmark.b.Run(testName, func(b *testing.B) {
-					report.reset()
-					l := ledger.Ledger{
-						Configuration: ledgerConfiguration,
-						Name:          uuid.NewString()[:8],
+				cpt := atomic.Int64{}
+
+				env := envFactory.Create(ctx, b, l)
+				b.Logf("ledger: %s/%s", l.Bucket, l.Name)
+
+				b.SetParallelism(int(parallelism))
+				b.ResetTimer()
+				b.RunParallel(func(pb *testing.PB) {
+					for pb.Next() {
+						iteration := int(cpt.Add(1))
+
+						script, vars := benchmark.Scripts[scriptName](iteration)
+						now := time.Now()
+						_, err := env.Executor().ExecuteScript(ctx, script, vars)
+						require.NoError(b, err)
+
+						report.registerTransactionLatency(time.Since(now))
 					}
-
-					cpt := atomic.Int64{}
-
-					env := envFactory.Create(ctx, b, l)
-					b.Logf("ledger: %s/%s", l.Bucket, l.Name)
-
-					b.SetParallelism(int(parallelism))
-					b.ResetTimer()
-					b.RunParallel(func(pb *testing.PB) {
-						for pb.Next() {
-							iteration := int(cpt.Add(1))
-
-							script, vars := benchmark.Scripts[scriptName](iteration)
-							now := time.Now()
-							_, err := env.Executor().ExecuteScript(ctx, script, vars)
-							require.NoError(b, err)
-
-							report.registerTransactionLatency(time.Since(now))
-						}
-					})
-					b.StopTimer()
-					report.End = time.Now()
-
-					b.ReportMetric(report.TPS(), "t/s")
-					b.ReportMetric(float64(report.AverageDuration().Milliseconds()), "ms/transaction")
-
-					stopContext, cancel := context.WithTimeout(ctx, 10*time.Second)
-					b.Cleanup(cancel)
-
-					require.NoError(benchmark.b, env.Stop(stopContext))
 				})
+				b.StopTimer()
+				report.End = time.Now()
 
-				if report.Tachymeter.Count > 0 {
-					reports[scriptName] = append(reports[scriptName], report)
-				}
+				b.ReportMetric(report.TPS(), "t/s")
+				b.ReportMetric(float64(report.Tachymeter.Calc().Time.Avg.Milliseconds()), "ms/transaction")
+
+				stopContext, cancel := context.WithTimeout(ctx, 10*time.Second)
+				b.Cleanup(cancel)
+
+				require.NoError(benchmark.b, env.Stop(stopContext))
+			})
+
+			if report.Tachymeter.Count > 0 {
+				reports[scriptName] = append(reports[scriptName], report)
 			}
 		}
 	}
@@ -90,11 +88,11 @@ func (benchmark *Benchmark) Run(ctx context.Context) map[string][]Report {
 	return reports
 }
 
-func New(b *testing.B, envFactories map[string]EnvFactory, scripts map[string]func(int) (string, map[string]string)) *Benchmark {
+func New(b *testing.B, envFactory EnvFactory, scripts map[string]func(int) (string, map[string]string)) *Benchmark {
 	return &Benchmark{
-		b:            b,
-		EnvFactories: envFactories,
-		Scripts:      scripts,
-		reports:      make(map[string]map[string]*Report),
+		b:          b,
+		EnvFactory: envFactory,
+		Scripts:    scripts,
+		reports:    make(map[string]map[string]*Report),
 	}
 }
