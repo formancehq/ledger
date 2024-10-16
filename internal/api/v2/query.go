bilby91/stack

feat(ledger): make stateless

components/ledger/internal/api/v2/query.go
@@ -1,13 +1,13 @@
 package v2
 
 import (
+	"github.com/formancehq/ledger/internal/controller/ledger/writer"
 	"net/http"
 	"strings"
 
 	"github.com/formancehq/stack/libs/go-libs/api"
 	"github.com/formancehq/stack/libs/go-libs/bun/bunpaginate"
 
-	"github.com/formancehq/ledger/internal/engine/command"
 	"github.com/pkg/errors"
 )
 
@@ -25,11 +25,11 @@ var (
 	ErrInvalidEndTime   = errors.New("invalid 'endTime' query param")
 )
 
-func getCommandParameters(r *http.Request) command.Parameters {
+func getCommandParameters(r *http.Request) writer.Parameters {
 	dryRunAsString := r.URL.Query().Get("dryRun")
 	dryRun := strings.ToUpper(dryRunAsString) == "YES" || strings.ToUpper(dryRunAsString) == "TRUE" || dryRunAsString == "1"
 
-	return command.Parameters{
+	return writer.Parameters{
 		DryRun:         dryRun,
 		IdempotencyKey: api.IdempotencyKeyFromRequest(r),
 	}

formancehq/ledger

internal/api/v2/query.go
@@ -2,13 +2,11 @@ package v2
 
 import (
 	"net/http"
-	"strings"
+
+	"github.com/formancehq/ledger/internal/controller/ledger"
 
 	"github.com/formancehq/go-libs/api"
 	"github.com/formancehq/go-libs/bun/bunpaginate"
-
-	"github.com/formancehq/ledger/internal/engine/command"
-	"github.com/pkg/errors"
 )
 
 const (
@@ -18,19 +16,9 @@ const (
 	QueryKeyCursor = "cursor"
 )
 
-var (
-	ErrInvalidBalanceOperator = errors.New(
-		"invalid parameter 'balanceOperator', should be one of 'e, ne, gt, gte, lt, lte'")
-	ErrInvalidStartTime = errors.New("invalid 'startTime' query param")
-	ErrInvalidEndTime   = errors.New("invalid 'endTime' query param")
-)
-
-func getCommandParameters(r *http.Request) command.Parameters {
-	dryRunAsString := r.URL.Query().Get("dryRun")
-	dryRun := strings.ToUpper(dryRunAsString) == "YES" || strings.ToUpper(dryRunAsString) == "TRUE" || dryRunAsString == "1"
-
-	return command.Parameters{
-		DryRun:         dryRun,
+func getCommandParameters(r *http.Request) ledger.Parameters {
+	return ledger.Parameters{
+		DryRun:         api.QueryParamBool(r, "dryRun"),
 		IdempotencyKey: api.IdempotencyKeyFromRequest(r),
 	}
 }