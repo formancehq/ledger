c318745d2568c85b0e09b476b89d7b2b589406c3
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-19 17:10:19 +0200

    feat: make migrations starting asynchronoulsy at application startup

--- internal/storage/bucket/migrations.go
+++ internal/storage/bucket/migrations.go
@@ -2,6 +2,7 @@ package bucket
 
 import (
 	"context"
+	"database/sql"
 	"embed"
 	"github.com/formancehq/go-libs/v2/migrations"
 	"github.com/uptrace/bun"
@@ -13,7 +14,26 @@ var migrationsDir embed.FS
 
 func GetMigrator(name string) *migrations.Migrator {
 	migrator := migrations.NewMigrator(migrations.WithSchema(name, true))
-	migrator.RegisterMigrationsFromFileSystem(migrationsDir, "migrations")
+	migrations, err := migrations.CollectMigrationFiles(migrationsDir, "migrations")
+	if err != nil {
+		panic(err)
+	}
+
+	for ind, migration := range migrations[:12] {
+		originalUp := migration.Up
+		migration.Up = func(ctx context.Context, db bun.IDB) error {
+			return db.RunInTx(ctx, &sql.TxOptions{}, func(ctx context.Context, tx bun.Tx) error {
+				_, err := tx.ExecContext(ctx, "set search_path to '"+name+"'")
+				if err != nil {
+					return err
+				}
+				return originalUp(ctx, tx)
+			})
+		}
+		migrations[ind] = migration
+	}
+
+	migrator.RegisterMigrations(migrations...)
 
 	return migrator
 }
