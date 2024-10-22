fb84de9bd9335db11e5606536783971373af5f34
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2024-10-22 11:00:17 +0200

    fix: recreate account index concurrently

--- internal/storage/bucket/migrations/20-clean-database/up.sql
+++ internal/storage/bucket/migrations/20-clean-database/up.sql
@@ -74,3 +74,53 @@ drop column accounts_seq;
 
 alter table transactions
 drop column seq;
+
+alter table accounts
+drop column seq;
+
+-- rename index create in previous migration, as the drop of the column seq of accounts table has automatically dropped the index accounts_ledger
+alter index accounts_ledger2
+rename to accounts_ledger;
+
+create or replace function set_log_hash()
+	returns trigger
+	security definer
+	language plpgsql
+as
+$$
+declare
+	previousHash bytea;
+	marshalledAsJSON varchar;
+begin
+	select hash into previousHash
+	from logs
+	where ledger = new.ledger
+	order by id desc
+	limit 1;
+
+	-- select only fields participating in the hash on the backend and format json representation the same way
+	select '{' ||
+		'"type":"' || new.type || '",' ||
+		'"data":' || encode(new.memento, 'escape') || ',' ||
+		'"date":"' || (to_json(new.date::timestamp)#>>'{}') || 'Z",' ||
+		'"idempotencyKey":"' || coalesce(new.idempotency_key, '') || '",' ||
+		'"id":0,' ||
+		'"hash":null' ||
+   '}' into marshalledAsJSON;
+
+	new.hash = (
+		select public.digest(
+			case
+			when previousHash is null
+			then marshalledAsJSON::bytea
+			else '"' || encode(previousHash::bytea, 'base64')::bytea || E'"\n' || convert_to(marshalledAsJSON, 'LATIN1')::bytea
+			end || E'\n', 'sha256'::text
+		)
+	);
+
+	return new;
+end;
+$$ set search_path from current;
+
+alter table logs
+drop column seq;
