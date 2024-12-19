1560f5473bebb8757358f667be31791707331b9a
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2024-12-19 19:46:50 +0100

    fix: invalid benchmark scripts (#622)

--- test/performance/scripts/any_bounded_to_any.js
+++ test/performance/scripts/any_bounded_to_any.js
@@ -1,19 +1,23 @@
 function next() {
-    return {
-        action: 'CREATE_TRANSACTION',
-        data: {
-            plain: `vars {
-                account $source
-                account $destination
-            }
-            send [USD/2 100] (
-                source = $source allowing overdraft up to [USD/2 100]
-                destination = $destination
-            )`,
-            vars: {
-                destination: "dst:" + uuid(),
-                source: "src:" + uuid()
+    return [
+        {
+            action: 'CREATE_TRANSACTION',
+            data: {
+                script: {
+                    plain: `vars {
+                        account $source
+                        account $destination
+                    }
+                    send [USD/2 100] (
+                        source = $source allowing overdraft up to [USD/2 100]
+                        destination = $destination
+                    )`,
+                    vars: {
+                        destination: "dst:" + uuid(),
+                        source: "src:" + uuid()
+                    }
+                }
             }
         }
-    }
+    ]
 }
--- test/performance/scripts/world_to_bank.js
+++ test/performance/scripts/world_to_bank.js
@@ -1,12 +1,16 @@
 function next() {
-    return {
-        action: 'CREATE_TRANSACTION',
-        data: {
-            plain: `send [USD/2 100] (
-                source = @world
-                destination = @bank
-            )`,
-            vars: {}
+    return [
+        {
+            action: 'CREATE_TRANSACTION',
+            data: {
+                script: {
+                    plain: `send [USD/2 100] (
+                        source = @world
+                        destination = @bank
+                    )`,
+                    vars: {}
+                }
+            }
         }
-    }
+    ]
 }
