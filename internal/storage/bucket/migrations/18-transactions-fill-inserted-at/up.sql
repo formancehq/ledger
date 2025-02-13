a3482293e330f38e49966c675f4f25f79b7965b9
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-02-13 10:10:25 +0100

    fix: migration postgres azure (#681)
    * fix: migration using azure postgres with no superuser provileges
    
    * fix: next migration

--- internal/storage/bucket/migrations/18-transactions-fill-inserted-at/up.sql
+++ internal/storage/bucket/migrations/18-transactions-fill-inserted-at/up.sql
@@ -1,11 +1,20 @@
 do $$
 	declare
+		_ledger record;
+		_vsql text;
 		_batch_size integer := 1000;
 		_date timestamp without time zone;
 		_count integer := 0;
 	begin
-		--todo: take explicit advisory lock to avoid concurrent migrations when the service is killed
 		set search_path = '{{.Schema}}';
+
+		-- cannot disable triggers at session level on Azure Postgres with no superuser privileges.
+		-- so we modify the trigger acting on transaction update to be triggered only if the metadata column is updated.
+		-- by the way, it's a good move to not trigger the update_transaction_metadata_history function on every update if not necessary.
+		for _ledger in select * from _system.ledgers where bucket = current_schema loop
+			_vsql = 'create or replace trigger "update_transaction_metadata_history_' || _ledger.id || '" after update of metadata on "transactions" for each row when (new.ledger = ''' || _ledger.name || ''') execute procedure update_transaction_metadata_history()';
+			execute _vsql;
+		end loop;
 
 		-- select the date where the "11-make-stateless" migration has been applied
 		select tstamp into _date
@@ -25,9 +34,6 @@ do $$
 		perform pg_notify('migrations-{{ .Schema }}', 'init: ' || _count);
 
 		for i in 0.._count-1 by _batch_size loop
-			-- disable triggers
-			set session_replication_role = replica;
-
 			with _rows as (
 				select *
 				from logs_transactions
@@ -39,9 +45,6 @@ do $$
 			set inserted_at = _rows.date
 			from _rows
 			where transactions.ledger = _rows.ledger and transactions.id = _rows.transaction_id;
-
-			-- enable triggers
-			set session_replication_role = default;
 
 			commit;
 
