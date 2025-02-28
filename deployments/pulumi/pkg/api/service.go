0c13bcfbbbaa8da1a94707b6c453dd2634432540
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-02-28 14:04:55 +0100

    feat: refine pulumi

--- deployments/pulumi/pkg/api/service.go
+++ deployments/pulumi/pkg/api/service.go
@@ -1,18 +1,25 @@
-package pulumi_ledger
+package api
 
 import (
+	"github.com/formancehq/ledger/deployments/pulumi/pkg/utils"
+	v1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apps/v1"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/core/v1"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func installService(ctx *pulumi.Context, cmp *Component, args ComponentArgs) (*corev1.Service, error) {
+type createServiceArgs struct {
+	utils.CommonArgs
+	Deployment *v1.Deployment
+}
+
+func createService(ctx *pulumi.Context, args createServiceArgs, opts ...pulumi.ResourceOption) (*corev1.Service, error) {
 	return corev1.NewService(ctx, "ledger", &corev1.ServiceArgs{
 		Metadata: &metav1.ObjectMetaArgs{
 			Namespace: args.Namespace.ToOutput(ctx.Context()).Untyped().(pulumi.StringOutput),
 		},
 		Spec: &corev1.ServiceSpecArgs{
-			Selector: cmp.ServerDeployment.Spec.Selector().MatchLabels(),
+			Selector: args.Deployment.Spec.Selector().MatchLabels(),
 			Type:     pulumi.String("ClusterIP"),
 			Ports: corev1.ServicePortArray{
 				corev1.ServicePortArgs{
@@ -23,5 +30,5 @@ func installService(ctx *pulumi.Context, cmp *Component, args ComponentArgs) (*c
 				},
 			},
 		},
-	}, pulumi.Parent(cmp))
+	}, opts...)
 }
