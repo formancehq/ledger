d38461ea3d586b030b0d77213c8711477035b5b5
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-03-28 10:21:49 +0100

    fix: invalid post commit volumes format (#829)

---
+++ internal/storage/bucket/migrations/19-transactions-fill-pcv/up_tests_after.sql
@@ -0,0 +1,10 @@
+do $$
+	declare
+		expected varchar = '{"fees": {"USD": {"input": 1, "output": 0}}, "world": {"USD": {"input": 0, "output": 100}}, "orders:0": {"USD": {"input": 100, "output": 100}}, "sellers:0": {"USD": {"input": 99, "output": 0}}}';
+	begin
+		set search_path = '{{.Schema}}';
+		assert (select post_commit_volumes::varchar from transactions where id = 0) = expected,
+			'post_commit_volumes should be equals to ' || expected || ' but was ' || (select to_jsonb(post_commit_volumes) from transactions where id = 0);
+	end;
+$$
+
