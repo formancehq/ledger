345d3bd1729d93126b822dbd80f5cb983c12059d
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-04-08 10:23:23 +0200

    fix(openapi): bulk response definition

--- pkg/client/models/components/v2bulkresponse.go
+++ pkg/client/models/components/v2bulkresponse.go
@@ -3,36 +3,28 @@
 package components
 
 type V2BulkResponse struct {
-	Data         []V2BulkElementResult `json:"data"`
-	ErrorCode    V2ErrorsEnum          `json:"errorCode"`
-	ErrorMessage string                `json:"errorMessage"`
-	Details      *string               `json:"details,omitempty"`
+	Data         []V2BulkElementResult `json:"data,omitempty"`
+	ErrorCode    *V2ErrorsEnum         `json:"errorCode,omitempty"`
+	ErrorMessage *string               `json:"errorMessage,omitempty"`
 }
 
 func (o *V2BulkResponse) GetData() []V2BulkElementResult {
 	if o == nil {
-		return []V2BulkElementResult{}
+		return nil
 	}
 	return o.Data
 }
 
-func (o *V2BulkResponse) GetErrorCode() V2ErrorsEnum {
+func (o *V2BulkResponse) GetErrorCode() *V2ErrorsEnum {
 	if o == nil {
-		return V2ErrorsEnum("")
+		return nil
 	}
 	return o.ErrorCode
 }
 
-func (o *V2BulkResponse) GetErrorMessage() string {
-	if o == nil {
-		return ""
-	}
-	return o.ErrorMessage
-}
-
-func (o *V2BulkResponse) GetDetails() *string {
+func (o *V2BulkResponse) GetErrorMessage() *string {
 	if o == nil {
 		return nil
 	}
-	return o.Details
+	return o.ErrorMessage
 }
