443274a9c0d89416042734010e38bc24aa4b3cbe
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-04-09 17:50:56 +0200

    fix: incorrect metadata on reversing transactions

---
+++ internal/storage/bucket/migrations/29-fix-invalid-metadata-on-reverts/up_tests_before.sql
@@ -0,0 +1,9 @@
+do $$
+	declare
+		expected varchar = '{"tax": "1%"}';
+	begin
+		set search_path = '{{.Schema}}';
+		assert (select metadata::varchar from transactions where id = 22 and ledger = 'ledger0') = expected,
+			'metadata should be equals to ' || expected || ' but was ' || (select to_jsonb(metadata) from transactions where id = 22 and ledger = 'ledger0');
+	end;
+$$
