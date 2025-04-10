36eb52d500b189eecd7a8cb7169918f53f1b96da
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-04-10 07:28:51 +0200

    fix: backport get balances from main

--- internal/storage/ledger/balances.go
+++ internal/storage/ledger/balances.go
@@ -2,6 +2,7 @@ package ledger
 
 import (
 	"context"
+	"github.com/formancehq/ledger/internal/storage/bucket"
 	"math/big"
 	"slices"
 	"strings"
@@ -15,6 +16,20 @@ import (
 )
 
 func (store *Store) GetBalances(ctx context.Context, query ledgercontroller.BalanceQuery) (ledgercontroller.Balances, error) {
+	isUpToDate, err := bucket.NewDefault(store.db, store.tracer, store.ledger.Bucket).
+		IsUpToDate(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if isUpToDate {
+		return store.getBalancesAfterUpgrade(ctx, query)
+	} else {
+		return store.getBalancesWhenUpgrading(ctx, query)
+	}
+}
+
+func (store *Store) getBalancesWhenUpgrading(ctx context.Context, query ledgercontroller.BalanceQuery) (ledgercontroller.Balances, error) {
 	return tracing.TraceWithMetric(
 		ctx,
 		"GetBalances",
@@ -121,6 +136,105 @@ func (store *Store) GetBalances(ctx context.Context, query ledgercontroller.Bala
 				Model(&accountsVolumes)
 
 			err := finalQuery.Scan(ctx)
+			if err != nil {
+				return nil, postgres.ResolveError(err)
+			}
+
+			ret := ledgercontroller.Balances{}
+			for _, volumes := range accountsVolumes {
+				if _, ok := ret[volumes.Account]; !ok {
+					ret[volumes.Account] = map[string]*big.Int{}
+				}
+				ret[volumes.Account][volumes.Asset] = new(big.Int).Sub(volumes.Input, volumes.Output)
+			}
+
+			// Fill empty balances with 0 value
+			for account, assets := range query {
+				if _, ok := ret[account]; !ok {
+					ret[account] = map[string]*big.Int{}
+				}
+				for _, asset := range assets {
+					if _, ok := ret[account][asset]; !ok {
+						ret[account][asset] = big.NewInt(0)
+					}
+				}
+			}
+
+			return ret, nil
+		},
+	)
+}
+
+func (store *Store) getBalancesAfterUpgrade(ctx context.Context, query ledgercontroller.BalanceQuery) (ledgercontroller.Balances, error) {
+	return tracing.TraceWithMetric(
+		ctx,
+		"GetBalances",
+		store.tracer,
+		store.getBalancesHistogram,
+		func(ctx context.Context) (ledgercontroller.Balances, error) {
+			conditions := make([]string, 0)
+			args := make([]any, 0)
+			for account, assets := range query {
+				for _, asset := range assets {
+					conditions = append(conditions, "ledger = ? and accounts_address = ? and asset = ?")
+					args = append(args, store.ledger.Name, account, asset)
+				}
+			}
+
+			type AccountsVolumesWithLedger struct {
+				ledger.AccountsVolumes `bun:",extend"`
+				Ledger                 string `bun:"ledger,type:varchar"`
+			}
+
+			accountsVolumes := make([]AccountsVolumesWithLedger, 0)
+			for account, assets := range query {
+				for _, asset := range assets {
+					accountsVolumes = append(accountsVolumes, AccountsVolumesWithLedger{
+						Ledger: store.ledger.Name,
+						AccountsVolumes: ledger.AccountsVolumes{
+							Account: account,
+							Asset:   asset,
+							Input:   new(big.Int),
+							Output:  new(big.Int),
+						},
+					})
+				}
+			}
+
+			// prevent deadlocks by sorting the accountsVolumes slice
+			slices.SortStableFunc(accountsVolumes, func(i, j AccountsVolumesWithLedger) int {
+				if i.Account < j.Account {
+					return -1
+				} else if i.Account > j.Account {
+					return 1
+				} else if i.Asset < j.Asset {
+					return -1
+				} else if i.Asset > j.Asset {
+					return 1
+				} else {
+					return 0
+				}
+			})
+
+			err := store.db.NewSelect().
+				With(
+					"ins",
+					// Try to insert volumes with 0 values.
+					// This way, if the account has a 0 balance at this point, it will be locked as any other accounts.
+					// It the complete sql transaction fail, the account volumes will not be inserted.
+					store.db.NewInsert().
+						Model(&accountsVolumes).
+						ModelTableExpr(store.GetPrefixedRelationName("accounts_volumes")).
+						On("conflict do nothing"),
+				).
+				Model(&accountsVolumes).
+				ModelTableExpr(store.GetPrefixedRelationName("accounts_volumes")).
+				Column("accounts_address", "asset", "input", "output").
+				Where("("+strings.Join(conditions, ") OR (")+")", args...).
+				For("update").
+				// notes(gfyrag): Keep order, it ensures consistent locking order and limit deadlocks
+				Order("accounts_address", "asset").
+				Scan(ctx)
 			if err != nil {
 				return nil, postgres.ResolveError(err)
 			}
