cf50144bb0dd752da82b27732a14b846bd342c5a
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-05-22 11:22:38 +0200

    fix: image-pull-policy ignored (#940)

--- deployments/pulumi/pkg/config/config.go
+++ deployments/pulumi/pkg/config/config.go
@@ -660,7 +660,7 @@ func Load(ctx *pulumi.Context) (*Config, error) {
 		}
 	}
 
-	timeout, err := config.TryInt(ctx, "timeout")
+	timeout, err := cfg.TryInt("timeout")
 	if err != nil {
 		if errors.Is(err, config.ErrMissingVar) {
 			timeout = 60
@@ -670,7 +670,7 @@ func Load(ctx *pulumi.Context) (*Config, error) {
 	}
 
 	storage := &Storage{}
-	if err := config.GetObject(ctx, "storage", storage); err != nil {
+	if err := cfg.GetObject("storage", storage); err != nil {
 		if !errors.Is(err, config.ErrMissingVar) {
 			return nil, fmt.Errorf("error reading storage config: %w", err)
 		}
@@ -678,21 +678,21 @@ func Load(ctx *pulumi.Context) (*Config, error) {
 	}
 
 	api := &API{}
-	if err := config.GetObject(ctx, "api", api); err != nil {
+	if err := cfg.GetObject("api", api); err != nil {
 		if !errors.Is(err, config.ErrMissingVar) {
 			return nil, fmt.Errorf("error reading api config: %w", err)
 		}
 	}
 
 	worker := &Worker{}
-	if err := config.GetObject(ctx, "worker", worker); err != nil {
+	if err := cfg.GetObject("worker", worker); err != nil {
 		if !errors.Is(err, config.ErrMissingVar) {
 			return nil, fmt.Errorf("error reading worker config: %w", err)
 		}
 	}
 
 	monitoring := &Monitoring{}
-	if err := config.GetObject(ctx, "monitoring", monitoring); err != nil {
+	if err := cfg.GetObject("monitoring", monitoring); err != nil {
 		if !errors.Is(err, config.ErrMissingVar) {
 			return nil, fmt.Errorf("error reading monitoring config: %w", err)
 		}
@@ -713,7 +713,7 @@ func Load(ctx *pulumi.Context) (*Config, error) {
 		generator = nil
 	}
 
-	namespace := config.Get(ctx, "namespace")
+	namespace := cfg.Get("namespace")
 	if namespace == "" {
 		namespace = ctx.Stack()
 	}
@@ -721,12 +721,13 @@ func Load(ctx *pulumi.Context) (*Config, error) {
 	return &Config{
 		Timeout: timeout,
 		Common: Common{
-			Debug:      config.GetBool(ctx, "debug"),
-			Namespace:  namespace,
-			Tag:        config.Get(ctx, "version"),
-			Monitoring: monitoring,
+			Debug:           cfg.GetBool("debug"),
+			Namespace:       namespace,
+			Tag:             cfg.Get("version"),
+			Monitoring:      monitoring,
+			ImagePullPolicy: cfg.Get("image-pull-policy"),
 		},
-		InstallDevBox: config.GetBool(ctx, "install-dev-box"),
+		InstallDevBox: cfg.GetBool("install-dev-box"),
 		Storage:       storage,
 		API:           api,
 		Worker:        worker,
