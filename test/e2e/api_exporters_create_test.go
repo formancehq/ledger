ecf09506ab9eb0037cb555371183052a82ea9311
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-07-17 13:35:41 +0200

    test: add integration test for coverage

--- test/e2e/api_exporters_create_test.go
+++ test/e2e/api_exporters_create_test.go
@@ -4,6 +4,7 @@ package test_suite
 
 import (
 	"github.com/formancehq/go-libs/v3/logging"
+	"github.com/formancehq/go-libs/v3/testing/api"
 	. "github.com/formancehq/go-libs/v3/testing/deferred/ginkgo"
 	"github.com/formancehq/go-libs/v3/testing/platform/natstesting"
 	"github.com/formancehq/go-libs/v3/testing/platform/pgtesting"
@@ -41,29 +42,42 @@ var _ = Context("Exporters API tests", func() {
 			createExporterResponse *operations.V2CreateExporterResponse
 			err                    error
 		)
-		BeforeEach(func() {
-			createExporterRequest = components.V2ExporterConfiguration{
-				Driver: "clickhouse",
-				Config: map[string]any{
-					"dsn": "clickhouse://localhost:9000",
-				},
-			}
-		})
-		BeforeEach(func(specContext SpecContext) {
+		JustBeforeEach(func(specContext SpecContext) {
 			createExporterResponse, err = Wait(specContext, DeferClient(testServer)).Ledger.V2.CreateExporter(ctx, createExporterRequest)
 		})
-		It("should be ok", func() {
-			Expect(err).To(BeNil())
-		})
-		Context("then deleting it", func() {
-			BeforeEach(func(specContext SpecContext) {
-				Expect(err).To(BeNil())
-				_, err = Wait(specContext, DeferClient(testServer)).Ledger.V2.DeleteExporter(ctx, operations.V2DeleteExporterRequest{
-					ExporterID: createExporterResponse.V2CreateExporterResponse.Data.ID,
-				})
+		Context("with invalid configuration", func() {
+			BeforeEach(func() {
+				createExporterRequest = components.V2ExporterConfiguration{
+					Driver: "clickhouse",
+				}
 			})
-			It("Should be ok", func() {
+			It("should return an error", func() {
+				Expect(err).ToNot(BeNil())
+				Expect(err).To(api.HaveErrorCode(string(components.V2ErrorsEnumValidation)))
+			})
+		})
+		Context("with valid configuration", func() {
+			BeforeEach(func() {
+				createExporterRequest = components.V2ExporterConfiguration{
+					Driver: "clickhouse",
+					Config: map[string]any{
+						"dsn": "clickhouse://localhost:9000",
+					},
+				}
+			})
+			It("should be ok", func() {
 				Expect(err).To(BeNil())
+			})
+			Context("then deleting it", func() {
+				BeforeEach(func(specContext SpecContext) {
+					Expect(err).To(BeNil())
+					_, err = Wait(specContext, DeferClient(testServer)).Ledger.V2.DeleteExporter(ctx, operations.V2DeleteExporterRequest{
+						ExporterID: createExporterResponse.V2CreateExporterResponse.Data.ID,
+					})
+				})
+				It("Should be ok", func() {
+					Expect(err).To(BeNil())
+				})
 			})
 		})
 	})
