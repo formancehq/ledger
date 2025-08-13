424b4c1b0fc41562a7304d9b9085df835e5d84df
Azorlogh <bott.alix@gmail.com>
2025-08-13 14:53:25 +0200

    k8s test setup

--- test/antithesis/image/entrypoint.sh
+++ test/antithesis/image/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # make sure pg is ready to accept connections
-until pg_isready -d ledger -h 10.0.29.16 -U ledger
+until pg_isready -d ledger -h postgres -U ledger
 do
   echo "Waiting for postgres at: $POSTGRES_URI"
   sleep 2;
