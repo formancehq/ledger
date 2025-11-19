043a9301fc0c1fd8b05799bd3bdb6587d744e897
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-11-19 13:52:16 +0100

    fix: pulumi postgres chart version (#1141)

--- deployments/pulumi/pkg/config/config.go
+++ deployments/pulumi/pkg/config/config.go
@@ -152,6 +152,9 @@ type PostgresInstall struct {
 
 	// Password is the password for the Postgres database
 	Password string `json:"password" yaml:"password"`
+
+	// ChartVersion is the bitnami/postgres chart version
+	ChartVersion string `json:"chartVersion" yaml:"chartVersion"`
 }
 
 type PostgresDatabase struct {
@@ -180,6 +183,7 @@ func (a *PostgresDatabase) toInput() *storage.PostgresDatabaseArgs {
 		Install: &storage.PostgresInstallArgs{
 			Username: pulumix.Val(a.Install.Username),
 			Password: pulumix.Val(a.Install.Password),
+			ChartVersion: pulumix.Val(a.Install.ChartVersion),
 		},
 	}
 }
