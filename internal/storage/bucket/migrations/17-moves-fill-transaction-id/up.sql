ca2af50ee6c1e224949ebef4b1ec9c83f38b9a41
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2025-11-24 15:36:08 +0100

    experiment: improve migration performance

--- internal/storage/bucket/migrations/17-moves-fill-transaction-id/up.sql
+++ internal/storage/bucket/migrations/17-moves-fill-transaction-id/up.sql
@@ -5,40 +5,38 @@ do $$
 	begin
 		set search_path = '{{.Schema}}';
 
-		select count(seq)
+		create temporary table transactions_ids as
+		select row_number() over (order by transactions.seq) as row_number,
+		       moves.seq as moves_seq, transactions.id, transactions.seq as transactions_seq
 		from moves
-		where transactions_id is null
+		join transactions on transactions.seq = moves.transactions_seq
+		where transactions_id is null;
+
+		create index transactions_ids_rows on transactions_ids(row_number) include (moves_seq, transactions_seq, id);
+
+		analyze transactions_ids;
+
+		select count(*)
+		from transactions_ids
 		into _max;
 
 		perform pg_notify('migrations-{{ .Schema }}', 'init: ' || _max);
-		loop
 
-			with _outdated_moves as (
+		for i in 0.._max-1 by _batch_size loop
+			with _rows as (
 				select *
-				from moves
-				where transactions_id is null
-				limit _batch_size
+				from transactions_ids
+				where row_number >= i and row_number < i + _batch_size
 			)
 			update moves
-			set transactions_id = (
-				select id
-				from transactions
-				where seq = moves.transactions_seq
-			)
-			from _outdated_moves
-			where moves.seq in (_outdated_moves.seq);
+			set transactions_id = _rows.id
+			from _rows
+			where seq = _rows.moves_seq;
 
-			exit when not found;
+			commit;
 
 			perform pg_notify('migrations-{{ .Schema }}', 'continue: ' || _batch_size);
-
-			commit ;
 		end loop;
-
-		alter table moves
-		add constraint transactions_id_not_null
-		check (transactions_id is not null)
-		not valid;
 	end
 $$
 language plpgsql;
