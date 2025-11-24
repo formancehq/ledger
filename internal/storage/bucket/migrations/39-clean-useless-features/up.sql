9809958fe300bc9ddab12cdc568ca25a171abe39
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-11-24 09:32:04 +0100

    fix: migrations with pg13 (#1146)

--- internal/storage/bucket/migrations/39-clean-useless-features/up.sql
+++ internal/storage/bucket/migrations/39-clean-useless-features/up.sql
@@ -9,13 +9,23 @@ do $$
 		-- this way, the 2.2 (which use triggers) continuer to work, and the 2.3 which does not need them can work too
 		-- todo(next minor / 2.4): remove triggers and associated functions
 		for ledger in select * from _system.ledgers where bucket = current_schema loop
-			vsql = 'create or replace trigger "transaction_set_addresses_' || ledger.id || '" before insert on transactions for each row when (new.ledger = ''' || ledger.name || ''' and new.sources is null) execute procedure set_transaction_addresses()';
+
+			vsql = 'drop trigger "transaction_set_addresses_' || ledger.id || '" on transactions';
 			execute vsql;
 
-			vsql = 'create or replace trigger "accounts_set_address_array_' || ledger.id || '" before insert on accounts for each row when (new.ledger = ''' || ledger.name || ''' and new.address_array is null) execute procedure set_address_array_for_account()';
+			vsql = 'create trigger "transaction_set_addresses_' || ledger.id || '" before insert on transactions for each row when (new.ledger = ''' || ledger.name || ''' and new.sources is null) execute procedure set_transaction_addresses()';
 			execute vsql;
 
-			vsql = 'create or replace trigger "transaction_set_addresses_segments_' || ledger.id || '"	before insert on "transactions" for each row when (new.ledger = ''' || ledger.name || ''' and new.sources_arrays is null) execute procedure set_transaction_addresses_segments()';
+			vsql = 'drop trigger "accounts_set_address_array_' || ledger.id || '" on accounts';
+			execute vsql;
+
+			vsql = 'create trigger "accounts_set_address_array_' || ledger.id || '" before insert on accounts for each row when (new.ledger = ''' || ledger.name || ''' and new.address_array is null) execute procedure set_address_array_for_account()';
+			execute vsql;
+
+			vsql = 'drop trigger "transaction_set_addresses_segments_' || ledger.id || '" on transactions';
+			execute vsql;
+
+			vsql = 'create trigger "transaction_set_addresses_segments_' || ledger.id || '"	before insert on "transactions" for each row when (new.ledger = ''' || ledger.name || ''' and new.sources_arrays is null) execute procedure set_transaction_addresses_segments()';
 			execute vsql;
 		end loop;
 
