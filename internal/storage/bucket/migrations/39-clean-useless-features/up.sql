f8b2138ac0b826e07113503833a43fc86d3763f6
Ragot Geoffrey <geoffrey.ragot@gmail.com>
2025-11-27 18:54:52 +0100

    chore: clean db before 2.4 (#1159)

--- internal/storage/bucket/migrations/39-clean-useless-features/up.sql
+++ internal/storage/bucket/migrations/39-clean-useless-features/up.sql
@@ -7,7 +7,6 @@ do $$
 
 		-- recreate trigger but with a check on sources is null for transactions and address_array is null for accounts
 		-- this way, the 2.2 (which use triggers) continuer to work, and the 2.3 which does not need them can work too
-		-- todo(next minor / 2.4): remove triggers and associated functions
 		for ledger in select * from _system.ledgers where bucket = current_schema loop
 
 			vsql = 'drop trigger "transaction_set_addresses_' || ledger.id || '" on transactions';
@@ -28,25 +27,6 @@ do $$
 			vsql = 'create trigger "transaction_set_addresses_segments_' || ledger.id || '"	before insert on "transactions" for each row when (new.ledger = ''' || ledger.name || ''' and new.sources_arrays is null) execute procedure set_transaction_addresses_segments()';
 			execute vsql;
 		end loop;
-
-		-- todo(next minor / 2.4): remove triggers and associated functions
--- 		for ledger in select * from _system.ledgers where bucket = current_schema loop
--- 			vsql = 'drop trigger if exists "transaction_set_addresses_' || ledger.id || '" on transactions';
--- 			execute vsql;
---
--- 			vsql = 'drop trigger if exists "accounts_set_address_array_' || ledger.id || '" on accounts';
--- 			execute vsql;
---
--- 			vsql = 'drop trigger if exists "transaction_set_addresses_segments_' || ledger.id || '" on transactions';
--- 			execute vsql;
--- 		end loop;
---
--- 		commit;
---
--- 		drop function set_transaction_addresses;
--- 		drop function set_transaction_addresses_segments;
--- 		drop function set_address_array_for_account;
--- 		drop function explode_address;
 	end
 $$;
 
