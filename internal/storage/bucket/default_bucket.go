9bc402d6709dbfe0411d9ff085c60df90a4460d8
Azorlogh <bott.alix@gmail.com>
2025-12-08 17:21:41 +0100

    fix migration number

--- internal/storage/bucket/default_bucket.go
+++ internal/storage/bucket/default_bucket.go
@@ -18,7 +18,7 @@ import (
 )
 
 // stateless version (+1 regarding directory name, as migrations start from 1 in the lib)
-const MinimalSchemaVersion = 43
+const MinimalSchemaVersion = 26
 
 type DefaultBucket struct {
 	name string
