2f91490346e4db8ec9317d359a6f5ebcfd39d18b
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2026-01-23 09:53:26 +0100

    feat: add replica identity on tables without primary key

--- internal/storage/bucket/default_bucket.go
+++ internal/storage/bucket/default_bucket.go
@@ -18,7 +18,7 @@ import (
 )
 
 // stateless version (+1 regarding directory name, as migrations start from 1 in the lib)
-const MinimalSchemaVersion = 48
+const MinimalSchemaVersion = 49
 
 type DefaultBucket struct {
 	name string
