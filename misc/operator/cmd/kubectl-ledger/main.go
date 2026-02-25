0ca5d3bbe397c760bf81b383378864fcc8f8964e
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2026-02-25 14:22:03 +0100

    fix: print error message when kubectl-ledger commands fail
    SilenceErrors on the root cobra command suppressed error output,
    causing commands like "create" without arguments to exit silently.

--- misc/operator/cmd/kubectl-ledger/main.go
+++ misc/operator/cmd/kubectl-ledger/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	if err := newRootCommand().Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
 		os.Exit(1)
 	}
 }
